b5860418791303b302d98697e185388d585acf30
Alexander Echavarria Calle <alex.haec.4711@gmail.com>
2019-08-08 13:04:26 +0200

    added routes to login and binnacle

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import LoginPage from './pages/login/LoginPage';
+import BinnaclePage from './pages/binnacle/BinnaclePage';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Login</Link>
+            </li>
+            <li>
+              <Link to='/binnacle/'>Binnacle</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path='/' exact component={LoginPage} />
+        <Route path='/binnacle/' component={BinnaclePage} />
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
