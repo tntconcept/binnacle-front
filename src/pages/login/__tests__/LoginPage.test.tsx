9ddf7dd154a6e406b382320277cb8db6577be1ce
Emanuel Suca <esuca@autentia.com>
2019-08-09 07:44:08 +0200

    mocks the entire react-i18next library to return the key instead of the translated text

---
+++ src/pages/login/__tests__/LoginPage.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {render} from "@testing-library/react"
+import LoginPage from "../LoginPage"
+
+it('should render correctly', function () {
+    const result = render(<LoginPage/>)
+
+    expect(result.asFragment().firstElementChild).toHaveTextContent("Login page")
+})
