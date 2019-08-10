887ffd1b728c1595b693ec03169c68f354fbb20a
Emanuel Suca <esuca@autentia.com>
2019-08-11 00:58:20 +0200

    adds the basic eslint configuration

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import {useTranslation} from 'react-i18next'
+import React, {useEffect, useState} from "react"
+import {useTranslation} from "react-i18next"
 
 const LoginPage: React.FC = () => {
-  const {t} = useTranslation()
+  const { t } = useTranslation();
 
-  return <h1>Login {t('page')}</h1>
+  const [a, setA] = useState(false);
+
+  console.log("2");
+  useEffect(() => {
+    if (a) {
+      setA(false);
+    }
+  }, [a]);
+
+  console.log("");
+
+  return (
+    <React.Fragment>
+      <h1>Login {t("page")}</h1>;
+      <img
+        src="nada"
+        alt="" />
+    </React.Fragment>
+  );
 };
 
 export default LoginPage;
