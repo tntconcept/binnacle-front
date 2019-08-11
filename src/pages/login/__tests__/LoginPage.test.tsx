69e5e574661d043589257af86d04cf93d8972cb2
Emanuel Suca <esuca@autentia.com>
2019-08-11 21:58:53 +0200

    removes __tests__ folder of login page for now

--- src/pages/login/__tests__/LoginPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import LoginPage from "../LoginPage";
-
-it("should render correctly", function() {
-  const result = render(<LoginPage />);
-
-  expect(result.asFragment().firstElementChild).toHaveTextContent("Login page");
-});
