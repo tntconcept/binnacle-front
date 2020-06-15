c40ab9c30e52e280410073f81e80868fd6c8a9c5
Emanuel Suca <esuca@autentia.com>
2020-06-15 17:11:29 +0200

    chore: enables PWA mode on production

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import * as serviceWorker from "./serviceWorker"
-import App from "app"
-import "i18n"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+import App from 'app'
+import 'i18n'
 import 'what-input'
 
 // @ts-ignore
-ReactDOM.unstable_createRoot(document.getElementById("root")).render(<App/>)
+ReactDOM.unstable_createRoot(document.getElementById('root')).render(<App />)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.register()
