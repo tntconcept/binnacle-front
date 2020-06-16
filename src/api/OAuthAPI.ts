ffc33af988a0c334798f5fba622e8b4a1db86bbe
Emanuel Suca <esuca@autentia.com>
2020-06-16 19:40:20 +0200

    build: refactors OAuth login api and production client secret

--- src/api/OAuthAPI.ts
+++ src/api/OAuthAPI.ts
@@ -1,43 +1,49 @@
-import {IOAuth} from "api/interfaces/IOAuth"
-import {baseHttpClient} from "services/HttpClient"
-import {TokenService} from "services/TokenService"
-import endpoints from "api/endpoints"
+import { IOAuth } from 'api/interfaces/IOAuth'
+import { baseHttpClient } from 'services/HttpClient'
+import { TokenService } from 'services/TokenService'
+import endpoints from 'api/endpoints'
 
 const OAuthClientCredentials = {
   username: process.env.REACT_APP_CLIENT_NAME,
   password: process.env.REACT_APP_CLIENT_SECRET
 }
 
-const base64Credentials = btoa(OAuthClientCredentials.username + ":" + OAuthClientCredentials.password)
+const base64Credentials = btoa(
+  OAuthClientCredentials.username + ':' + OAuthClientCredentials.password
+)
 
 export const login = async (username: string, password: string) => {
-  const response =  await baseHttpClient.post(endpoints.auth, {
-    searchParams: {
-      grant_type: "password",
-      username: username,
-      password: password
-    },
-    headers: {
-      Authorization: `Basic ${base64Credentials}`
-    },
-  }).json<IOAuth>()
+  const searchParams = new URLSearchParams()
+  searchParams.set('grant_type', 'password')
+  searchParams.set('username', username)
+  searchParams.set('password', password)
+
+  const response = await baseHttpClient
+    .post(endpoints.auth, {
+      body: searchParams,
+      headers: {
+        Authorization: `Basic ${base64Credentials}`
+      }
+    })
+    .json<IOAuth>()
 
   TokenService.storeTokens(response.access_token, response.refresh_token)
   return response
-};
+}
 
 export const refreshToken = async () => {
-  const response = await baseHttpClient.post(endpoints.auth, {
-    headers: {
-      Authorization: `Basic ${base64Credentials}`
-    },
-    searchParams: {
-      grant_type: "refresh_token",
-      refresh_token: TokenService.getTokens().refresh_token!
-    }
-  }).json<IOAuth>()
+  const response = await baseHttpClient
+    .post(endpoints.auth, {
+      headers: {
+        Authorization: `Basic ${base64Credentials}`
+      },
+      searchParams: {
+        grant_type: 'refresh_token',
+        refresh_token: TokenService.getTokens().refresh_token!
+      }
+    })
+    .json<IOAuth>()
 
   TokenService.storeTokens(response.access_token, response.refresh_token)
   return response
-};
-
+}
--- src/utils/__tests__/helpers.test.ts
+++ src/utils/__tests__/helpers.test.ts
@@ -1,18 +1,17 @@
-import {cls, rem, roundToTwoDecimals} from "utils/helpers"
+import { cls, rem, roundToTwoDecimals } from 'utils/helpers'
 
-test("should parse px to rem", () => {
-  expect(rem("16px")).toEqual("1rem")
-  expect(rem("32px")).toEqual("2rem")
-  expect(rem("18px")).toEqual("1.125rem")
+test('should parse px to rem', () => {
+  expect(rem('16px')).toEqual('1rem')
+  expect(rem('32px')).toEqual('2rem')
+  expect(rem('18px')).toEqual('1.125rem')
 })
 
-test("should conditionally join classes", () => {
+test('should conditionally join classes', () => {
   expect(cls('base', false && 'never', true && 'test')).toBe('base test')
 })
 
-test("should round to two decimals", () => {
-  expect(roundToTwoDecimals(3.2540)).toBe(3.25)
-  expect(roundToTwoDecimals(3.2550)).toBe(3.26)
-  expect(roundToTwoDecimals(3.2580)).toBe(3.26)
+test('should round to two decimals', () => {
+  expect(roundToTwoDecimals(3.254)).toBe(3.25)
+  expect(roundToTwoDecimals(3.255)).toBe(3.26)
+  expect(roundToTwoDecimals(3.258)).toBe(3.26)
 })
-
