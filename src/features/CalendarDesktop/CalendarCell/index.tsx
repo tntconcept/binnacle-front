7121642ae377ef988691f8db74aa9c38fe0d3611
Emanuel Suca <esuca@autentia.com>
2020-07-15 11:58:25 +0200

    chore: adds missing changes of previous commit

--- src/features/CalendarDesktop/CalendarCell/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from 'features/CalendarDesktop/CalendarCell/CalendarCell.module.css'
-
-const Cell: React.FC = (props) => {
-  return <div className={styles.base}>{props.children}</div>
-}
-
-export default Cell
---
+++ src/pages/binnacle/CalendarDesktop/index.tsx
@@ -0,0 +1,1 @@
+export { default } from 'pages/binnacle/CalendarDesktop/CalendarDesktop'
