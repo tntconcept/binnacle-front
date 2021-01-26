7cd5f3cf94b7f34ccf4db939fe57d31ea277b60b
Emanuel Suca <esuca@autentia.com>
2021-01-26 16:41:02 +0100

    feat: update automatically the service worker

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { ServiceWorkerUpdateBanner } from 'core/components/ServiceWorkerUpdateBanner'
 import { IOSInstallPWAPrompt } from 'core/components/IOSInstallPWAPrompt'
 import { AppRoutes } from 'core/AppRoutes'
 import { AppProviders } from 'core/AppProviders'
@@ -17,7 +16,6 @@ const App: React.FC = () => {
 
   return (
     <AppProviders>
-      <ServiceWorkerUpdateBanner />
       <IOSInstallPWAPrompt />
       <AppRoutes />
     </AppProviders>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'core/App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 // @ts-ignore
 // Enable Concurrent Mode
@@ -10,4 +11,7 @@ ReactDOM.unstable_createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
-// Service worker is enabled inside the <ServiceWorkerUpdateBanner /> component!
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
