949b842614a6ed97b5276946afb388309819c567
Laura Cercas <laura.cercas@autentia.com>
2022-02-04 11:02:22 +0100

    feat:181123242 Refactor

--- src/modules/binnacle/components/ActivityForm/utils/get-activity-http-error-message.ts
+++ src/modules/binnacle/components/ActivityForm/utils/get-activity-http-error-message.ts
@@ -1,32 +1,34 @@
 import i18n from 'shared/i18n/i18n'
+import { CodeErrors } from '../../../../../shared/types/error-codes'
 
 export const getActivityHttpErrorMessage = (error: any) => {
   let notificationErrorMessage = undefined
 
   if (error.response && error.response.status === 400) {
-    if (error.response.data.code === 'ACTIVITY_TIME_OVERLAPS') {
-      notificationErrorMessage = {
-        400: {
-          title: i18n.t('activity_api_errors.time_overlaps_title'),
-          description: i18n.t('activity_api_errors.time_overlaps_description')
+    const code = error.response.data.code
+
+    switch (code) {
+      case CodeErrors.ACTIVITY_TIME_OVERLAPS:
+        return notificationErrorMessage = {
+          400: {
+            title: i18n.t('activity_api_errors.time_overlaps_title'),
+            description: i18n.t('activity_api_errors.time_overlaps_description')
+          }
         }
-      }
-    } else if (error.response.data.code === 'CLOSED_PROJECT') {
-      notificationErrorMessage = {
-        400: {
-          title: i18n.t('activity_api_errors.closed_project_title'),
-          description: i18n.t('activity_api_errors.closed_project_description')
+      case CodeErrors.CLOSED_PROJECT:
+        return notificationErrorMessage = {
+          400: {
+            title: i18n.t('activity_api_errors.closed_project_title'),
+            description: i18n.t('activity_api_errors.closed_project_description')
+          }
         }
-      }
-    } else if (error.response.data.code === 'ACTIVITY_PERIOD_CLOSED') {
-      notificationErrorMessage = {
-        400: {
-          title: i18n.t('activity_api_errors.activity_closed_period_title'),
-          description: i18n.t('activity_api_errors.activity_closed_period_description')
+      case CodeErrors.ACTIVITY_PERIOD_CLOSED:
+        return notificationErrorMessage = {
+          400: {
+            title: i18n.t('activity_api_errors.activity_closed_period_title'),
+            description: i18n.t('activity_api_errors.activity_closed_period_description')
+          }
         }
-      }
     }
   }
-
-  return notificationErrorMessage
 }
