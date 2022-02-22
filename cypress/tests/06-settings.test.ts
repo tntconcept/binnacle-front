a5f4641564bd849e50783d623341fd5190fb6ecb
Kemil Beltre <kemil.beltre@autentia.com>
2022-02-22 15:36:30 +0100

    test: add cypress test for theme

--- cypress/tests/06-settings.test.ts
+++ cypress/tests/06-settings.test.ts
@@ -1,17 +1,26 @@
 import SettingsPO from '../page-objects/SettingsPO'
 
 describe('Settings page', () => {
-  it('should change the language', function() {
+  it('should change the language', function () {
     cy.smartLoginTo('settings')
 
-    cy.findByLabelText('Language')
-      .find('option:selected')
-      .should('have.text', 'English')
+    cy.findByLabelText('Language').find('option:selected').should('have.text', 'English')
 
     SettingsPO.changeLanguage('es')
     cy.contains('Idioma').should('be.visible')
 
     SettingsPO.changeLanguage('en')
     cy.contains('Language').should('be.visible')
+  })
+
+  it('should change the theme', async () => {
+    cy.smartLoginTo('settings')
+
+    cy.findByLabelText('Theme')
+      .find('option:selected')
+      .should('have.text', 'Automatic (based on system theme)')
+
+    SettingsPO.changeTheme('Dark')
+    cy.contains('Dark').should('be.visible')
   })
 })
