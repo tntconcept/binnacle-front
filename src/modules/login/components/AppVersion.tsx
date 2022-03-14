f5375261b131cbc7cfc907ec727c51e66978e0c6
Kemil Beltre <kemil.beltre@autentia.com>
2022-03-14 15:57:20 +0100

    feat: add api version in UI

--- src/modules/login/components/AppVersion.tsx
+++ src/modules/login/components/AppVersion.tsx
@@ -1,10 +1,22 @@
 import { Text } from '@chakra-ui/react'
 import appVersion from '../../../../package.json'
+import { useGlobalState } from '../../../shared/arch/hooks/use-global-state'
+import { AppState } from '../../../shared/data-access/state/app-state'
 
 export const AppVersion = () => {
+  const { apiVersion } = useGlobalState(AppState)
+
   return (
-    <Text p={2} alignSelf="flex-end">
-      v{appVersion.version}
-    </Text>
+    <>
+      <Text p={2} alignSelf="flex-end">
+        v{appVersion.version}
+      </Text>
+
+      {apiVersion ? (
+        <Text p={2} alignSelf="flex-end">
+          API: v{apiVersion}
+        </Text>
+      ) : null}
+    </>
   )
 }
