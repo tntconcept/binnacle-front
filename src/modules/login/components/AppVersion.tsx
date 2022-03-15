071ba386b0260d57a30e5e62d4260c2e404242a2
Kemil Beltre <kemil.beltre@autentia.com>
2022-03-15 13:44:31 +0100

    fix: show api version when is not loading

--- src/modules/login/components/AppVersion.tsx
+++ src/modules/login/components/AppVersion.tsx
@@ -11,7 +11,7 @@ export const AppVersion = () => {
 
   return (
     <Text p={2} alignSelf="flex-end">
-      app: v{appVersion.version} {isLoading ? `| api:  v${apiVersion}` : null}
+      app: v{appVersion.version} {isLoading ? null : `| api: v${apiVersion}`}
     </Text>
   )
 }
