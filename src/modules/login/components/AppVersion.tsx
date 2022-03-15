c534f366894b37499e9ac9d85807286bad750128
Kemil Beltre <kemil.beltre@autentia.com>
2022-03-15 09:02:32 +0100

    feat: add api version in UI (use action)

--- src/modules/login/components/AppVersion.tsx
+++ src/modules/login/components/AppVersion.tsx
@@ -2,9 +2,12 @@ import { Text } from '@chakra-ui/react'
 import appVersion from '../../../../package.json'
 import { useGlobalState } from '../../../shared/arch/hooks/use-global-state'
 import { AppState } from '../../../shared/data-access/state/app-state'
+import { useActionOnMount } from '../../../shared/arch/hooks/use-action-on-mount'
+import { GetApiVersionAction } from '../data-access/actions/get-api-version-action'
 
 export const AppVersion = () => {
   const { apiVersion } = useGlobalState(AppState)
+  const isLoading = useActionOnMount(GetApiVersionAction)
 
   return (
     <>
@@ -12,7 +15,7 @@ export const AppVersion = () => {
         v{appVersion.version}
       </Text>
 
-      {apiVersion ? (
+      {isLoading ? (
         <Text p={2} alignSelf="flex-end">
           API: v{apiVersion}
         </Text>
