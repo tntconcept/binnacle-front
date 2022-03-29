6cbccdf45ba40529e2f51e0a1ae0a1f26e97bd61
Kemil Beltre <kemil.beltre@autentia.com>
2022-03-29 17:30:20 +0200

    refactor: remove unnecessary variable

--- src/modules/binnacle/components/ActivityForm/utils/get-activity-http-error-message.ts
+++ src/modules/binnacle/components/ActivityForm/utils/get-activity-http-error-message.ts
@@ -2,41 +2,38 @@ import i18n from 'shared/i18n/i18n'
 import { CodeErrors } from '../../../../../shared/types/error-codes'
 
 export const getActivityHttpErrorMessage = (error: any) => {
-  let notificationErrorMessage = undefined
-
   if (error.response && error.response.status === 400) {
     const code = error.response.data.code
 
     switch (code) {
       case CodeErrors.ACTIVITY_TIME_OVERLAPS:
-        return (notificationErrorMessage = {
+        return {
           400: {
             title: i18n.t('activity_api_errors.time_overlaps_title'),
             description: i18n.t('activity_api_errors.time_overlaps_description')
           }
-        })
+        }
       case CodeErrors.CLOSED_PROJECT:
-        return (notificationErrorMessage = {
+        return {
           400: {
             title: i18n.t('activity_api_errors.closed_project_title'),
             description: i18n.t('activity_api_errors.closed_project_description')
           }
-        })
+        }
       case CodeErrors.ACTIVITY_PERIOD_CLOSED:
-        return (notificationErrorMessage = {
+        return {
           400: {
             title: i18n.t('activity_api_errors.activity_closed_period_title'),
             description: i18n.t('activity_api_errors.activity_closed_period_description')
           }
-        })
+        }
       case CodeErrors.ACTIVITY_BEFORE_HIRING_DATE:
-        /* eslint-disable  @typescript-eslint/no-unused-vars */
-        return (notificationErrorMessage = {
+        return {
           400: {
             title: i18n.t('activity_api_errors.activity_before_hiring_date_title'),
             description: i18n.t('activity_api_errors.activity_before_hiring_date_description')
           }
-        })
+        }
     }
   }
 }
--- src/modules/vacations/components/VacationFormModal/get-vacation-error-message.ts
+++ src/modules/vacations/components/VacationFormModal/get-vacation-error-message.ts
@@ -2,48 +2,45 @@ import i18n from 'shared/i18n/i18n'
 import { CodeErrors } from '../../../../shared/types/error-codes'
 
 export const getVacationErrorMessage = (error: any) => {
-  let notificationErrorMessage = undefined
-
   if (error.response && error.response.status === 400) {
     const code = error.response.data.code
 
     switch (code) {
       case CodeErrors.INVALID_NEXT_YEAR_VACATION_DAYS_REQUEST:
-        return (notificationErrorMessage = {
+        return {
           400: {
             title: i18n.t('vacation.error_max_vacation_days_requested_next_year_title'),
             description: i18n.t('vacation.error_max_vacation_days_requested_next_year_description')
           }
-        })
+        }
       case CodeErrors.VACATION_RANGE_CLOSED:
-        return (notificationErrorMessage = {
+        return {
           400: {
             title: i18n.t('vacation.error_vacation_range_closed_title'),
             description: i18n.t('vacation.error_vacation_range_closed_description')
           }
-        })
+        }
       case CodeErrors.VACATION_BEFORE_HIRING_DATE:
-        return (notificationErrorMessage = {
+        return {
           400: {
             title: i18n.t('vacation.error_vacation_before_hiring_date_title'),
             description: i18n.t('vacation.error_vacation_before_hiring_date_description')
           }
-        })
+        }
       case CodeErrors.VACATION_REQUEST_OVERLAPS:
-        return (notificationErrorMessage = {
+        return {
           400: {
             title: i18n.t('vacation.error_vacation_request_overlaps_title'),
             description: i18n.t('vacation.error_vacation_request_overlaps_description')
           }
-        })
+        }
       case CodeErrors.VACATION_REQUEST_EMPTY:
-        /* eslint-disable  @typescript-eslint/no-unused-vars */
-        return (notificationErrorMessage = {
+        return {
           400: {
             title: i18n.t('vacation.error_vacation_request_empty_title'),
             description: i18n.t('vacation.error_vacation_request_empty_description')
           }
-        })
+        }
     }
   }
 }
