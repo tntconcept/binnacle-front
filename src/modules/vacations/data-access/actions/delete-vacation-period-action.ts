30e57bf654f4f89709ac97f3019c1c72dd280d79
Kemil Beltre <kemil.beltre@autentia.com>
2022-04-06 09:22:09 +0200

    feat: add alert when a requested period has been removed

--- src/modules/vacations/data-access/actions/delete-vacation-period-action.ts
+++ src/modules/vacations/data-access/actions/delete-vacation-period-action.ts
@@ -1,14 +1,18 @@
 import { action, makeObservable } from 'mobx'
 import { GetVacationsByYearAction } from 'modules/vacations/data-access/actions/get-vacations-by-year-action'
 import { VacationsRepository } from 'modules/vacations/data-access/repositories/vacations-repository'
-import { singleton } from 'tsyringe'
+import { inject, singleton } from 'tsyringe'
 import type { IAction } from 'shared/arch/interfaces/IAction'
+import i18n from 'i18next'
+import { TOAST } from '../../../../shared/data-access/ioc-container/ioc-container.types'
+import type { ToastType } from '../../../../shared/data-access/ioc-container/ioc-container'
 
 @singleton()
 export class DeleteVacationPeriodAction implements IAction<number> {
   constructor(
     private vacationsRepository: VacationsRepository,
-    private getVacationsByYearAction: GetVacationsByYearAction
+    private getVacationsByYearAction: GetVacationsByYearAction,
+    @inject(TOAST) private toast: ToastType
   ) {
     makeObservable(this)
   }
@@ -16,6 +20,14 @@ export class DeleteVacationPeriodAction implements IAction<number> {
   @action
   async execute(vacationId: number): Promise<void> {
     await this.vacationsRepository.deleteVacationPeriod(vacationId)
-    await this.getVacationsByYearAction.execute(undefined)
+    await this.getVacationsByYearAction.execute()
+
+    this.toast({
+      title: i18n.t('vacation.remove_vacation_notification'),
+      status: 'success',
+      duration: 10000,
+      isClosable: true,
+      position: 'top-right'
+    })
   }
 }
