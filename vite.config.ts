ca1ec42b29b09b7605fb6ea58ae78cf6bc8e4090
Valentín Valverde <valenreta.developer@gmail.com>
2022-11-02 18:05:01 +0100

    feat: add reaload service worker

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,65 @@ import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import env from 'vite-plugin-env-compatible'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import eslintPlugin from '@nabla/vite-plugin-eslint'
+import replace from '@rollup/plugin-replace'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  manifest: {
+    name: 'Binnacle',
+    short_name: 'Binnacle',
+
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192px.png', // <== don't add slash, for testing
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/pwa-512px.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512px.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ]
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html'
+  }
+}
+
+const replaceOptions = { __DATE__: new Date().toISOString() }
+const claims = process.env.CLAIMS === 'true'
+const reload = process.env.RELOAD_SW === 'true'
+const selfDestroying = process.env.SW_DESTROY === 'true'
+
+if (process.env.SW === 'true') {
+  pwaOptions.srcDir = 'src'
+  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
+  pwaOptions.strategies = 'injectManifest'
+}
+
+if (claims) pwaOptions.registerType = 'autoUpdate'
+
+if (reload) {
+  // @ts-expect-error just ignore
+  replaceOptions.__RELOAD_SW__ = 'true'
+}
+
+if (selfDestroying) pwaOptions.selfDestroying = selfDestroying
 
 export default defineConfig({
-  base: '/binnacle/',
+  base: '/',
   build: {
     outDir: 'build',
     target: 'esnext'
@@ -26,9 +80,9 @@ export default defineConfig({
     env({
       prefix: 'REACT_APP_'
     }),
-    VitePWA({
-      registerType: 'autoUpdate'
-    }),
+    VitePWA(pwaOptions),
+    replace(replaceOptions) as any,
+
     eslintPlugin()
   ]
 })
