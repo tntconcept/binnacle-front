054f1cfa9ebb8a9da9b5a33c965f4d265fe02c9d
Valentín Valverde <valenreta.developer@gmail.com>
2022-11-03 14:52:26 +0100

    fix: add i18n language support

--- src/shared/components/ServiceWorkerPrompt/ServiceWorkerPrompt.tsx
+++ src/shared/components/ServiceWorkerPrompt/ServiceWorkerPrompt.tsx
@@ -2,8 +2,11 @@ import './ServiceWorkerPrompt.css'
 
 import { Button, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay } from '@chakra-ui/react'
 import { useRegisterSW } from 'virtual:pwa-register/react'
+import { useTranslation } from 'react-i18next'
 
 export function ServiceWorkerPrompt() {
+  const { t } = useTranslation()
+
   // replaced dynamically
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const buildDate = '__DATE__'
@@ -36,12 +39,12 @@ export function ServiceWorkerPrompt() {
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>
-          <h2>Hay una nueva version de Binnacle 🎉</h2>
-          <span className="header__subtitle">Pulsa aceptar para recargar la página</span>
+          <h2>{t('service_worker.new_version.title')} 🎉</h2>
+          <span className="header__subtitle">{t('service_worker.new_version.subtitle')}</span>
         </ModalHeader>
         <ModalBody>
           <Button variant="outline" onClick={() => updateServiceWorker(true)}>
-            Aceptar
+            {t('service_worker.new_version.button')}
           </Button>
         </ModalBody>
       </ModalContent>
