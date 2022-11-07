699a78228e52a9480e7c1b0c6a99f48068a4462a
Valentín Valverde <valenreta.developer@gmail.com>
2022-11-07 09:42:14 +0100

    feat: assert past month

--- src/shared/utils/isPastDate.test.ts
+++ src/shared/utils/isPastDate.test.ts
@@ -2,9 +2,11 @@
  * [v] Should call isPastDate with a target date
  *
  * [v] Should return true if the target year is less than the current year
- * [] Should return false if the target year is gte the current year
+ * [v] Should return false if the target year is gte the current year
  *
- * [] Should return true if the target month is less than the current month
+ * [] Should return true if the target month is less than the current month and the target year is less than the current year
+ * [] Should return true if the target month is less than the current month and the target year is equals the current year
+ *
  * [] Should return false if the target month is gte the current month
  */
 
@@ -19,7 +21,28 @@ describe('isPastDate', () => {
 
   it('Should return false if the target year is greater than the current year', () => {
     const now = new Date()
-    const date = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1)
+    const date = new Date(now.getFullYear() + 1, now.getMonth(), now.getDate())
+    const result = isPastDate(date)
+    expect(result).toBe(false)
+  })
+
+  it('Should return true if year is equal and month is less than the current month', () => {
+    const now = new Date()
+    const date = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate())
+    const result = isPastDate(date)
+    expect(result).toBe(true)
+  })
+
+  it('Should return false if year is equal and month is equal', () => {
+    const now = new Date()
+    const date = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    const result = isPastDate(date)
+    expect(result).toBe(false)
+  })
+
+  it('Should return false if year is equal and month is greater', () => {
+    const now = new Date('2022-12-01')
+    const date = new Date(now.getFullYear(), now.getMonth() + 1, now.getDate())
     const result = isPastDate(date)
     expect(result).toBe(false)
   })
--- src/shared/utils/isPastDate.ts
+++ src/shared/utils/isPastDate.ts
@@ -1,8 +1,14 @@
 export const isPastDate = (targetDate: Date) => {
   const now = new Date()
   const year = now.getFullYear()
-
   const targetYear = targetDate.getFullYear()
 
-  return targetYear < year
+  const isPastYear = targetYear < year
+
+  const isSameYear = year === targetYear
+  const isPastMonth = targetDate.getMonth() < now.getMonth()
+
+  const isPast = isPastYear || (isSameYear && isPastMonth)
+
+  return isPast
 }
