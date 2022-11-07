bd8e964f307cd12f07213a95e778b554d3217d89
Valentín Valverde <valenreta.developer@gmail.com>
2022-11-07 09:47:34 +0100

    feat: show/hide recommend hours

--- src/modules/binnacle/components/WorkingTime/WorkingTime.tsx
+++ src/modules/binnacle/components/WorkingTime/WorkingTime.tsx
@@ -9,6 +9,7 @@ import { BinnacleState } from '../../data-access/state/binnacle-state'
 import { getDurationByHours } from '../../data-access/utils/getDuration'
 import chrono from '../../../../shared/utils/chrono'
 import { SelectWorkingTimeMode } from './SelectWorkingTimeMode'
+import { isPastDate } from 'shared/utils/isPastDate'
 
 export const WorkingTime = observer(() => {
   const { t } = useTranslation()
@@ -36,6 +37,12 @@ export const WorkingTime = observer(() => {
     setHourBalance(hourBalance)
     setIsNegativeBalance(hourBalance < 0)
   }, [worked, target])
+
+  const [isPastMonth, setIsPastMonth] = useState(false)
+
+  useEffect(() => {
+    setIsPastMonth(isPastDate(selectedDate))
+  }, [selectedDate])
 
   return (
     <Box
@@ -81,38 +88,44 @@ export const WorkingTime = observer(() => {
           </Text>
         </Box>
 
-        <Box textAlign="left" minWidth="55px">
-          {selectedWorkingTimeMode === 'by-year' ? (
-            <Text> {t('time_tracking.target_hours')}</Text>
-          ) : (
-            <Text>{t('time_tracking.recommended_hours')}</Text>
-          )}
-          <Text
-            data-testid="time_tracking_hours"
-            textTransform="initial"
-            fontWeight="600"
-            textAlign="left"
-            fontSize="sm"
-          >
-            {getDurationByHours(target ?? 0, settings.useDecimalTimeFormat)}
-          </Text>
-        </Box>
+        {!isPastMonth && (
+          <Box textAlign="left" minWidth="55px">
+            {selectedWorkingTimeMode === 'by-year' ? (
+              <Text> {t('time_tracking.target_hours')}</Text>
+            ) : (
+              <Text>{t('time_tracking.recommended_hours')}</Text>
+            )}
+            <Text
+              data-testid="time_tracking_hours"
+              textTransform="initial"
+              fontWeight="600"
+              textAlign="left"
+              fontSize="sm"
+            >
+              {getDurationByHours(target ?? 0, settings.useDecimalTimeFormat)}
+            </Text>
+          </Box>
+        )}
 
-        <Box textAlign="left" minWidth="55px">
-          <Text>Balance</Text>
-          <Text
-            textTransform="initial"
-            fontWeight="600"
-            textAlign="left"
-            fontSize="sm"
-            color={isNegativeBalance ? 'red.200' : 'green.200'}
-          >
-            <span aria-label={t(isNegativeBalance ? 'accessibility.minus' : 'accessibility.plus')}>
-              {isNegativeBalance ? '-' : '+'}
-            </span>
-            {getDurationByHours(Math.abs(hourBalance), settings.useDecimalTimeFormat)}
-          </Text>
-        </Box>
+        {!isPastMonth && (
+          <Box textAlign="left" minWidth="55px">
+            <Text>Balance</Text>
+            <Text
+              textTransform="initial"
+              fontWeight="600"
+              textAlign="left"
+              fontSize="sm"
+              color={isNegativeBalance ? 'red.200' : 'green.200'}
+            >
+              <span
+                aria-label={t(isNegativeBalance ? 'accessibility.minus' : 'accessibility.plus')}
+              >
+                {isNegativeBalance ? '-' : '+'}
+              </span>
+              {getDurationByHours(Math.abs(hourBalance), settings.useDecimalTimeFormat)}
+            </Text>
+          </Box>
+        )}
       </HStack>
     </Box>
   )
