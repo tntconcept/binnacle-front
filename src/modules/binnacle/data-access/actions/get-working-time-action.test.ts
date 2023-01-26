0cf8edec1e1eacb6ef3b0bf422004a0af2d1159f
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-01-26 11:27:35 +0100

    test: refactor WorkingTime unit tests with mocked data from generateTestMocks

--- src/modules/binnacle/data-access/actions/get-working-time-action.test.ts
+++ src/modules/binnacle/data-access/actions/get-working-time-action.test.ts
@@ -2,6 +2,7 @@ import { mock } from 'jest-mock-extended'
 import { BinnacleState } from '../state/binnacle-state'
 import { WorkingTimeRepository } from '../repositories/working-time-repository'
 import { GetWorkingTimeAction } from './get-working-time-action'
+import { mockWorkingTime } from 'test-utils/generateTestMocks'
 
 beforeEach(() => {
   jest.useFakeTimers('modern').setSystemTime(new Date('2022-02-01').getTime())
@@ -10,106 +11,19 @@ beforeEach(() => {
 describe('GetWorkingBalanceAction', () => {
   it('should get working time calculating the date from the binnacle state ', async () => {
     const { getWorkingTimeAction, workingTimeRepository, binnacleState } = setup()
-    workingTimeRepository.getWorkingTime.mockResolvedValue({
-      year: {
-        current: {
-          worked: 0,
-          target: 0,
-          balance: 0,
-          notRequestedVacations: 0
-        }
-      },
-      months: [
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        },
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        },
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        }
-      ]
-    })
+    workingTimeRepository.getWorkingTime.mockResolvedValue(mockWorkingTime())
     binnacleState.selectedDate = new Date('2022-01-01')
     const actualDate = new Date('2022-02-01')
 
     await getWorkingTimeAction.execute()
 
     expect(workingTimeRepository.getWorkingTime).toHaveBeenCalledWith(actualDate)
-    expect(binnacleState.workingTime).toEqual({
-      year: {
-        current: {
-          worked: 0,
-          target: 0,
-          balance: 0,
-          notRequestedVacations: 0
-        }
-      },
-      months: [
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        },
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        },
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        }
-      ]
-    })
+    expect(binnacleState.workingTime).toEqual(mockWorkingTime())
   })
 
   it('should get working time calculating the date from the selected month ', async () => {
     const { getWorkingTimeAction, workingTimeRepository, binnacleState } = setup()
-    workingTimeRepository.getWorkingTime.mockResolvedValue({
-      year: {
-        current: {
-          worked: 0,
-          target: 0,
-          balance: 0,
-          notRequestedVacations: 0
-        }
-      },
-      months: [
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        },
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        },
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        }
-      ]
-    })
+    workingTimeRepository.getWorkingTime.mockResolvedValue(mockWorkingTime())
     binnacleState.selectedDate = new Date('2022-01-01')
     const date = new Date('2021-12-01T00:00:00.000Z')
     const selectedMonth = new Date('2021-12-01')
@@ -118,36 +32,7 @@ describe('GetWorkingBalanceAction', () => {
     await getWorkingTimeAction.execute(selectedMonth, yearChanged)
 
     expect(workingTimeRepository.getWorkingTime).toHaveBeenCalledWith(date)
-    expect(binnacleState.workingTime).toEqual({
-      year: {
-        current: {
-          worked: 0,
-          target: 0,
-          balance: 0,
-          notRequestedVacations: 0
-        }
-      },
-      months: [
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        },
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        },
-        {
-          workable: 0,
-          worked: 0,
-          recommended: 0,
-          balance: 0
-        }
-      ]
-    })
+    expect(binnacleState.workingTime).toEqual(mockWorkingTime())
   })
 
   it('should make the request from january of the year selected when the year date is major than the actual date', async () => {
