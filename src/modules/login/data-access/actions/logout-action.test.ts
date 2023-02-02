0c73a0a934fd472d06dac88cb593d843c9f38d67
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-02-02 15:21:07 +0100

    feat: remove tokenStorage from LogoutAction

--- src/modules/login/data-access/actions/logout-action.test.ts
+++ src/modules/login/data-access/actions/logout-action.test.ts
@@ -1,12 +1,10 @@
-import { mock } from 'jest-mock-extended'
 import { LogoutAction } from 'modules/login/data-access/actions/logout-action'
-import { MemoryTokenStorage } from 'shared/api/oauth/token-storage/memory-token-storage'
 import { AppState } from 'shared/data-access/state/app-state'
 import { container } from 'tsyringe'
 
 describe('LogoutAction', () => {
   test('should logout', async () => {
-    const { tokenStorage, appState, logoutAction } = setup()
+    const { appState, logoutAction } = setup()
     appState.isAuthenticated = true
 
     expect(appState.isAuthenticated).toBe(true)
@@ -14,17 +12,14 @@ describe('LogoutAction', () => {
     await logoutAction.execute()
 
     expect(appState.isAuthenticated).toBe(false)
-    expect(tokenStorage.clearTokens).toHaveBeenCalled()
   })
 })
 
 function setup() {
-  const tokenStorage = mock<MemoryTokenStorage>()
   const appState = container.resolve(AppState)
 
   return {
-    tokenStorage,
     appState,
-    logoutAction: new LogoutAction(appState, tokenStorage)
+    logoutAction: new LogoutAction(appState)
   }
 }
