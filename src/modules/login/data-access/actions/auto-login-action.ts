1fae79c04974ca84ba42aa74037c1ad5731b9132
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-02-03 08:00:02 +0100

    feat: ignore AnonymousUserError for AutoLoginAction

--- src/modules/login/data-access/actions/auto-login-action.ts
+++ src/modules/login/data-access/actions/auto-login-action.ts
@@ -3,6 +3,7 @@ import { singleton } from 'tsyringe'
 import type { IAction } from 'shared/arch/interfaces/IAction'
 import { AppState } from 'shared/data-access/state/app-state'
 import { UserRepository } from 'modules/login/data-access/repositories/user-repository'
+import { AnonymousUserError } from '../errors/anonymous-user-error'
 
 @singleton()
 export class AutoLoginAction implements IAction<{ username: string; password: string }> {
@@ -12,13 +13,20 @@ export class AutoLoginAction implements IAction<{ username: string; password: st
 
   @action
   async execute(): Promise<void> {
-    const user = await this.userRepository.getUser()
+    try {
+      const user = await this.userRepository.getUser()
 
-    if (user) {
-      runInAction(() => {
-        this.appState.isAuthenticated = true
-        this.appState.loggedUser = user
-      })
+      if (user) {
+        runInAction(() => {
+          this.appState.isAuthenticated = true
+          this.appState.loggedUser = user
+        })
+      }
+    } catch (error) {
+      const isAnonymousUser = error instanceof AnonymousUserError
+      if (!isAnonymousUser) {
+        throw error
+      }
     }
   }
 }
