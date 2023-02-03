ce2e0c3a2ebf7f7d4d70d54f491001c014303696
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-02-03 07:58:17 +0100

    feat: now getUser can throw an AnonymousUserError

--- src/modules/login/data-access/repositories/user-repository.ts
+++ src/modules/login/data-access/repositories/user-repository.ts
@@ -2,12 +2,26 @@ import endpoints from 'shared/api/endpoints'
 import type { User } from 'shared/api/users/User'
 import { HttpClient } from 'shared/data-access/http-client/http-client'
 import { singleton } from 'tsyringe'
+import { AnonymousUserError } from '../errors/anonymous-user-error'
 
 @singleton()
 export class UserRepository {
   constructor(private httpClient: HttpClient) {}
 
   async getUser(): Promise<User> {
-    return await this.httpClient.get<User>(endpoints.user)
+    try {
+      const user = await this.httpClient.get<User>(endpoints.user)
+      return user
+    } catch (error) {
+      if (error.response?.status === 401) {
+        throw new AnonymousUserError()
+      }
+
+      throw error
+    }
+  }
+
+  async logout(): Promise<void> {
+    return this.httpClient.get<void>(endpoints.logout)
   }
 }
