5cc0c63dc91b1605c3ca90a36b0f49b0d6fd1d89
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-02-22 10:38:10 +0100

    feat: use ToastErrorNotification componente to create a notification with error

--- src/shared/components/Notifications/useShowErrorNotification.tsx
+++ src/shared/components/Notifications/useShowErrorNotification.tsx
@@ -2,6 +2,7 @@ import { useToast } from '@chakra-ui/react'
 import { useCallback } from 'react'
 import type { ICustomStatusMessages } from 'shared/components/Notifications/HttpStatusCodeMessage'
 import getMessageByHttpStatusCode from 'shared/components/Notifications/HttpStatusCodeMessage'
+import { ToastErrorNotification } from './toast-error-notification'
 
 export function useShowErrorNotification() {
   const toast = useToast()
@@ -11,12 +12,16 @@ export function useShowErrorNotification() {
       const message = getMessageByHttpStatusCode(error, overrideMessage)
 
       toast({
-        title: message.title,
-        description: message.description,
-        status: 'error',
         duration: process.env.NODE_ENV === 'test' ? 50 : 5000,
         isClosable: true,
-        position: 'top-right'
+        position: 'top-right',
+        render: (props) => (
+          <ToastErrorNotification
+            title={message.title}
+            description={message.description}
+            {...props}
+          />
+        )
       })
     },
     [toast]
