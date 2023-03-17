5328a6818811ccb067b902086ecbec956278e26c
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-03-17 12:29:44 +0100

    feat: use new object mothers in some repositories

--- src/modules/binnacle/data-access/repositories/fake-activity-repository.ts
+++ src/modules/binnacle/data-access/repositories/fake-activity-repository.ts
@@ -1,8 +1,4 @@
-import chrono from 'shared/utils/chrono'
-import {
-  buildActivityDaySummaryForMarch,
-  buildActivityWithProjectRoleId
-} from 'test-utils/generateTestMocks'
+import { ActivityMother } from 'test-utils/mothers/activity-mother'
 import { ActivityDaySummary } from '../interfaces/activity-day-summary'
 import { ActivityRepository } from '../interfaces/activity-repository'
 import { ActivityWithProjectRoleId } from '../interfaces/activity-with-project-role-id.interface'
@@ -11,73 +7,13 @@ import { RecentRole } from '../interfaces/recent-role'
 
 export class FakeActivityRepository implements ActivityRepository {
   async getActivitySummary(): Promise<ActivityDaySummary[]> {
-    return buildActivityDaySummaryForMarch()
+    return ActivityMother.marchActivitySummary()
   }
 
   async getActivities(): Promise<ActivityWithProjectRoleId[]> {
-    return [
-      buildActivityWithProjectRoleId({
-        projectRoleId: 123,
-        interval: {
-          start: chrono(new Date('2023-03-03 07:30')).getDate(),
-          end: chrono(new Date('2023-03-03 08:30')).getDate(),
-          timeUnit: 'MINUTES',
-          duration: 60
-        }
-      }),
-      buildActivityWithProjectRoleId({
-        projectRoleId: 123,
-        interval: {
-          start: chrono(new Date('2023-03-03 09:00')).getDate(),
-          end: chrono(new Date('2023-03-03 13:00')).getDate(),
-          timeUnit: 'MINUTES',
-          duration: 60
-        }
-      }),
-      buildActivityWithProjectRoleId({
-        projectRoleId: 123,
-        interval: {
-          start: chrono(new Date('2023-03-03 14:00')).getDate(),
-          end: chrono(new Date('2023-03-03 20:00')).getDate(),
-          timeUnit: 'MINUTES',
-          duration: 60
-        }
-      }),
-      buildActivityWithProjectRoleId({
-        projectRoleId: 123,
-        interval: {
-          start: chrono(new Date('2023-03-17 09:00:00')).getDate(),
-          end: chrono(new Date('2023-03-17 10:15:00')).getDate(),
-          timeUnit: 'MINUTES',
-          duration: 75
-        }
-      }),
-      buildActivityWithProjectRoleId({
-        projectRoleId: 456,
-        approvalState: 'PENDING'
-      }),
-      buildActivityWithProjectRoleId({
-        projectRoleId: 123,
-        interval: {
-          start: chrono(new Date('2023-03-23 00:00:00')).getDate(),
-          end: chrono(new Date('2023-03-30 00:00:00')).getDate(),
-          timeUnit: 'DAY',
-          duration: 6
-        },
-        approvalState: 'ACCEPTED',
-        hasEvidence: true
-      }),
-      buildActivityWithProjectRoleId({
-        projectRoleId: 123,
-        interval: {
-          start: chrono.now(),
-          end: chrono(new Date()).plus(1, 'hour').getDate(),
-          timeUnit: 'MINUTES',
-          duration: 60
-        }
-      })
-    ]
+    return ActivityMother.activitiesWithProjectRoleId()
   }
+
   getActivityImage(): Promise<string> {
     throw new Error('Method not implemented.')
   }
