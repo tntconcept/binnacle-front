9c561ef50b7a7b5ebe312916009b2809361a9d6e
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-03-23 12:14:39 +0100

    feat: new AuthContext to save a flag when user is logged in

---
+++ src/shared/contexts/auth-context.tsx
@@ -0,0 +1,39 @@
+import { GetUserLoggedQry } from 'features/user/application/get-user-logged-qry'
+import {
+  createContext,
+  Dispatch,
+  FC,
+  PropsWithChildren,
+  SetStateAction,
+  useContext,
+  useLayoutEffect,
+  useState
+} from 'react'
+import { useExecuteUseCaseOnMount } from 'shared/arch/hooks/use-execute-use-case-on-mount'
+
+type AuthState = {
+  isLoggedIn?: boolean
+  setIsLoggedIn?: Dispatch<SetStateAction<boolean | undefined>>
+}
+
+const AuthStateContext = createContext<AuthState>({})
+AuthStateContext.displayName = 'AuthStateContext'
+
+export const AuthProvider: FC<PropsWithChildren<AuthState>> = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>()
+  const { isLoading, result: userLogged } = useExecuteUseCaseOnMount(GetUserLoggedQry)
+
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      setIsLoggedIn(Boolean(userLogged))
+    }
+  }, [isLoading, userLogged])
+
+  return (
+    <AuthStateContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {!isLoading && props.children}
+    </AuthStateContext.Provider>
+  )
+}
+
+export const useAuthContext = (): AuthState => useContext(AuthStateContext)
