dbd706dc08b780251e41ff4e0c1075de7fff0092
Ignacio Gómez Oliva <ignacio.gomez.oliva@alumnos.upm.es>
2023-04-24 09:54:19 +0200

    test: get organization query and repository

---
+++ src/features/binnacle/features/organization/application/get-organizations-qry.test.ts
@@ -0,0 +1,26 @@
+import { mock } from 'jest-mock-extended'
+import { OrganizationRepository } from '../domain/organization-repository'
+import { GetOrganizationsQry } from './get-organizations-qry'
+import { OrganizationMother } from '../../../../../test-utils/mothers/organization-mother'
+
+describe('GetHolidaysQry', () => {
+  it('should get holidays from repository', async () => {
+    const { getOrganizationQry, organizationRepository } = setup()
+    const organizations = OrganizationMother.organizations()
+    organizationRepository.getAll.mockResolvedValue(organizations)
+
+    const response = await getOrganizationQry.internalExecute()
+
+    expect(organizationRepository.getAll).toHaveBeenCalled()
+    expect(response).toEqual(organizations)
+  })
+})
+
+function setup() {
+  const organizationRepository = mock<OrganizationRepository>()
+
+  return {
+    organizationRepository,
+    getOrganizationQry: new GetOrganizationsQry(organizationRepository)
+  }
+}
---
+++ src/features/binnacle/features/organization/infrastructure/http-organization-repository.test.ts
@@ -0,0 +1,26 @@
+import { mock } from 'jest-mock-extended'
+import { OrganizationMother } from '../../../../../test-utils/mothers/organization-mother'
+import { HttpClient } from '../../../../../shared/http/http-client'
+import { HttpOrganizationRepository } from './http-organization-repository'
+
+describe('HttpOrganizationRepository', () => {
+  it('should call http client for organizations', async () => {
+    const { httpClient, httpOrganizationRepository } = setup()
+    const organizations = OrganizationMother.organizations()
+    httpClient.get.mockResolvedValue(OrganizationMother.organizations())
+
+    const result = await httpOrganizationRepository.getAll()
+
+    expect(httpClient.get).toHaveBeenCalled()
+    expect(result).toEqual(organizations)
+  })
+})
+
+function setup() {
+  const httpClient = mock<HttpClient>()
+
+  return {
+    httpClient,
+    httpOrganizationRepository: new HttpOrganizationRepository(httpClient)
+  }
+}
