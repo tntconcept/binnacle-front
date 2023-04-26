1dd05527df18b4c9e010430492fa5a63c88359be
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-04-26 07:27:03 +0200

    feat: remove Toast dependency for CreateActivity and UpdateActivity commands

--- src/features/binnacle/features/activity/application/create-activity-cmd.ts
+++ src/features/binnacle/features/activity/application/create-activity-cmd.ts
@@ -1,7 +1,5 @@
 import { Command, UseCaseKey } from '@archimedes/arch'
-import i18n from 'i18next'
-import type { ToastType } from 'shared/di/container'
-import { ACTIVITY_REPOSITORY, TOAST } from 'shared/di/container-tokens'
+import { ACTIVITY_REPOSITORY } from 'shared/di/container-tokens'
 import { inject, singleton } from 'tsyringe'
 import type { ActivityRepository } from '../domain/activity-repository'
 import { NewActivity } from '../domain/new-activity'
@@ -9,21 +7,10 @@ import { NewActivity } from '../domain/new-activity'
 @UseCaseKey('CreateActivityCmd')
 @singleton()
 export class CreateActivityCmd extends Command<NewActivity> {
-  constructor(
-    @inject(ACTIVITY_REPOSITORY) private activityRepository: ActivityRepository,
-    @inject(TOAST) private toast: ToastType
-  ) {
+  constructor(@inject(ACTIVITY_REPOSITORY) private activityRepository: ActivityRepository) {
     super()
   }
   async internalExecute(newActivity: NewActivity): Promise<void> {
     await this.activityRepository.create(newActivity)
-
-    this.toast({
-      title: i18n.t('activity_form.create_activity_notification'),
-      status: 'success',
-      duration: 10000,
-      isClosable: true,
-      position: 'top-right'
-    })
   }
 }
--- src/features/binnacle/features/activity/application/update-activity-cmd.ts
+++ src/features/binnacle/features/activity/application/update-activity-cmd.ts
@@ -1,7 +1,5 @@
 import { Command, UseCaseKey } from '@archimedes/arch'
-import type { ToastType } from 'shared/di/container'
-import { ACTIVITY_REPOSITORY, TOAST } from 'shared/di/container-tokens'
-import i18n from 'shared/i18n/i18n'
+import { ACTIVITY_REPOSITORY } from 'shared/di/container-tokens'
 import { inject, singleton } from 'tsyringe'
 import type { ActivityRepository } from '../domain/activity-repository'
 import { UpdateActivity } from '../domain/update-activity'
@@ -9,21 +7,10 @@ import { UpdateActivity } from '../domain/update-activity'
 @UseCaseKey('UpdateActivityCmd')
 @singleton()
 export class UpdateActivityCmd extends Command<UpdateActivity> {
-  constructor(
-    @inject(ACTIVITY_REPOSITORY) private activityRepository: ActivityRepository,
-    @inject(TOAST) private toast: ToastType
-  ) {
+  constructor(@inject(ACTIVITY_REPOSITORY) private activityRepository: ActivityRepository) {
     super()
   }
   async internalExecute(activity: UpdateActivity): Promise<void> {
     await this.activityRepository.update(activity)
-
-    this.toast({
-      title: i18n.t('activity_form.update_activity_notification'),
-      status: 'success',
-      duration: 10000,
-      isClosable: true,
-      position: 'top-right'
-    })
   }
 }
