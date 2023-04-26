2a3e101318d3e58c664a8011052b3d5cb0e63696
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-04-26 07:26:36 +0200

    feat(archimedes): add ToastNotificationLink to Archimedes chain

--- src/shared/archimedes/archimedes.ts
+++ src/shared/archimedes/archimedes.ts
@@ -14,6 +14,7 @@ import { GetActivitySummaryQry } from 'features/binnacle/features/activity/appli
 import { GetTimeSummaryQry } from 'features/binnacle/features/activity/application/get-time-summary-qry'
 import { GetYearBalanceQry } from 'features/binnacle/features/activity/application/get-year-balance-qry'
 import { UpdateActivityCmd } from 'features/binnacle/features/activity/application/update-activity-cmd'
+import { GetProjectRolesQry } from 'features/binnacle/features/project-role/application/get-project-roles-qry'
 import { GetRecentProjectRolesQry } from 'features/binnacle/features/project-role/application/get-recent-project-roles-qry'
 import { SearchProjectRolesQry } from 'features/binnacle/features/search/application/search-project-roles-qry'
 import { CreateVacationCmd } from 'features/binnacle/features/vacation/application/create-vacation-cmd'
@@ -25,10 +26,16 @@ import { UpdateVacationCmd } from 'features/binnacle/features/vacation/applicati
 import { LogoutCmd } from 'features/user/application/logout-cmd'
 import { GetUserSettingsQry } from 'features/user/features/settings/application/get-user-settings-qry'
 import { SaveUserSettingsCmd } from 'features/user/features/settings/application/save-user-settings-cmd'
+import { ToastType } from 'shared/di/container'
+import { TOAST } from 'shared/di/container-tokens'
+import { container } from 'tsyringe'
+import { ToastNotificationLink } from './links/toast-notification-link'
 
+const toast = container.resolve<ToastType>(TOAST)
 Archimedes.createChain([
   new CacheLink(new CacheManager()),
   new ExecutorLink(),
+  new ToastNotificationLink(toast),
   new LoggerLink(console)
 ])
 
@@ -37,6 +44,7 @@ CacheInvalidations.set(LogoutCmd.prototype.key, [InvalidationPolicy.ALL])
 CacheInvalidations.set(SaveUserSettingsCmd.prototype.key, [GetUserSettingsQry.prototype.key])
 
 // Activities
+CacheInvalidations.set(GetProjectRolesQry.prototype.key, [InvalidationPolicy.NO_CACHE])
 CacheInvalidations.set(CreateActivityCmd.prototype.key, [
   GetActivitiesQry.prototype.key,
   GetActivitySummaryQry.prototype.key,
