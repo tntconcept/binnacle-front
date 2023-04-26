6ed05a1f5024d6ae09e66ac8553ddf4a12980110
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-04-26 16:11:07 +0200

    feat: new fields for LiteProjectRoleWithProjectId

--- src/features/binnacle/features/search/domain/lite-project-role-with-project-id.ts
+++ src/features/binnacle/features/search/domain/lite-project-role-with-project-id.ts
@@ -1,6 +1,9 @@
 import { Id } from 'shared/types/id'
 import { ProjectRole } from '../../project-role/domain/project-role'
 
-export type LiteProjectRoleWithProjectId = Pick<ProjectRole, 'id' | 'name'> & {
+export type LiteProjectRoleWithProjectId = Omit<
+  ProjectRole,
+  'organization' | 'project' | 'userId'
+> & {
   projectId: Id
 }
