17eba0ee38f7f918b2c9cd73a6e543867282589d
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-04-28 07:52:19 +0200

    fix: interfaces used on getHoliday and getVacation utils functions

--- src/features/binnacle/features/activity/utils/getHoliday.ts
+++ src/features/binnacle/features/activity/utils/getHoliday.ts
@@ -1,5 +1,5 @@
-import type { Holiday } from 'shared/types/Holiday'
 import chrono from 'shared/utils/chrono'
+import { Holiday } from '../../holiday/domain/holiday'
 
 export const getHoliday = (publicHolidays: Holiday[], date: Date) =>
   publicHolidays.find((holiday) => chrono(holiday.date).isSame(date, 'day'))
--- src/features/binnacle/features/activity/utils/getVacation.ts
+++ src/features/binnacle/features/activity/utils/getVacation.ts
@@ -1,5 +1,5 @@
-import type { Vacation } from 'shared/types/Vacation'
 import chrono from 'shared/utils/chrono'
+import { Vacation } from '../../vacation/domain/vacation'
 
 export const getVacation = (vacations: Vacation[], date: Date) =>
   vacations.find((vacation) => vacation.days.some((day) => chrono(day).isSame(date, 'day')))
