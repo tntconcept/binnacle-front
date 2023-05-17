b73cb6a07a56cae20a65147eeac5b2797626eea9
goyobjs <goyobujor@gmail.com>
2023-05-17 13:40:29 +0200

    test(http-activity-repository): test working

--- src/features/binnacle/features/activity/infrastructure/http-activity-repository.test.ts
+++ src/features/binnacle/features/activity/infrastructure/http-activity-repository.test.ts
@@ -2,6 +2,8 @@ import { mock } from 'jest-mock-extended'
 import { HttpClient } from 'shared/http/http-client'
 import { HttpActivityRepository } from './http-activity-repository'
 import { ActivityMother } from '../../../../../test-utils/mothers/activity-mother'
+import { Base64Converter } from '../../../../../shared/base64/base64-converter'
+import chrono, { parseISO } from '../../../../../shared/utils/chrono'
 
 describe('HttpActivityRepository', () => {
   test('should call http client for activities', async () => {
@@ -12,16 +14,29 @@ describe('HttpActivityRepository', () => {
 
     const result = await httpActivityRepository.getActivitySummary({ start, end })
 
-    expect(httpClient.get).toHaveBeenCalledWith()
-    expect(result).toEqual(ActivityMother.marchActivitySummary())
+    const parsedSummary = ActivityMother.marchActivitySummary().map((x) => {
+      return {
+        date: parseISO(chrono(x.date).format(chrono.DATE_FORMAT)),
+        worked: x.worked
+      }
+    })
+
+    expect(httpClient.get).toHaveBeenCalledWith('/api/activity/summary', {
+      params: {
+        endDate: chrono(end).format(chrono.DATE_FORMAT),
+        startDate: chrono(start).format(chrono.DATE_FORMAT)
+      }
+    })
+    expect(result).toEqual(parsedSummary)
   })
 })
 
 function setup() {
   const httpClient = mock<HttpClient>()
+  const base64Converter = mock<Base64Converter>()
 
   return {
     httpClient,
-    httpActivityRepository: new HttpActivityRepository(httpClient)
+    httpActivityRepository: new HttpActivityRepository(httpClient, base64Converter)
   }
 }
