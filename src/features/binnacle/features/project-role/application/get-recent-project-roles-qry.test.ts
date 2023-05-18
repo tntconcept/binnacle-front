f0b5381df3337b0ecfc398fe070db7712895f72b
Kemil Beltre <kemilbeltre@gmail.com>
2023-05-18 16:55:12 +0200

    test: add get-recent-project-roles-qry

---
+++ src/features/binnacle/features/project-role/application/get-recent-project-roles-qry.test.ts
@@ -0,0 +1,44 @@
+import { anyArray, mock } from 'jest-mock-extended'
+import { ProjectRoleMother } from 'test-utils/mothers/project-role-mother'
+import { SearchMother } from 'test-utils/mothers/search-mother'
+import { SearchProjectRolesQry } from '../../search/application/search-project-roles-qry'
+import { HydrateProjectRoles } from '../domain/hydrate-project-roles'
+import { ProjectRoleRepository } from '../domain/project-role-repository'
+import { GetRecentProjectRolesQry } from './get-recent-project-roles-qry'
+
+describe('GetRecentProjectRolesQry', () => {
+  it('should get recent roles', async () => {
+    const { getRecentProjectRolesQry, projectRoles } = setup()
+
+    const result = await getRecentProjectRolesQry.internalExecute()
+
+    expect(result).toEqual(projectRoles)
+  })
+})
+
+function setup() {
+  const projectRoleRepository = mock<ProjectRoleRepository>()
+  const searchProjectRolesQry = mock<SearchProjectRolesQry>()
+  const hydrateProjectRoles = mock<HydrateProjectRoles>()
+
+  const nonHydratedProjectRoles = ProjectRoleMother.nonHydratedProjectRoles()
+  projectRoleRepository.getRecents.mockResolvedValue(nonHydratedProjectRoles)
+
+  const seachResult = SearchMother.roles()
+  searchProjectRolesQry.execute.calledWith(anyArray()).mockResolvedValue(seachResult)
+
+  const projectRoles = ProjectRoleMother.projectRoles()
+  hydrateProjectRoles.hydrate
+    .calledWith(nonHydratedProjectRoles, seachResult)
+    .mockReturnValue(projectRoles)
+
+  return {
+    getRecentProjectRolesQry: new GetRecentProjectRolesQry(
+      projectRoleRepository,
+      searchProjectRolesQry,
+      hydrateProjectRoles
+    ),
+    projectRoleRepository,
+    projectRoles
+  }
+}
