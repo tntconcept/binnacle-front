633254709da10418b34cbcc365e938d4f67b9c36
Kemil Beltre <kemilbeltre@gmail.com>
2023-05-18 16:08:56 +0200

    test: add http-project-role-repository

---
+++ src/features/binnacle/features/project-role/infrastructure/http-project-role-repository.test.ts
@@ -0,0 +1,39 @@
+import { Id } from '@archimedes/arch'
+import { mock } from 'jest-mock-extended'
+import { HttpClient } from 'shared/http/http-client'
+import { ProjectRoleMother } from 'test-utils/mothers/project-role-mother'
+import { HttpProjectRoleRepository } from './http-project-role-repository'
+
+describe('HttpProjectRoleRepository', () => {
+  it('should get the recent roles', async () => {
+    const { httpProjectRoleRepository, projectRoleResponse } = setup()
+
+    const result = await httpProjectRoleRepository.getRecents()
+
+    expect(result).toEqual(projectRoleResponse)
+  })
+
+  it('should get all roles', async () => {
+    const { httpProjectRoleRepository, projectRoleResponse } = setup()
+
+    const result = await httpProjectRoleRepository.getAll(1)
+
+    expect(result).toEqual(projectRoleResponse)
+  })
+})
+
+function setup() {
+  const httpClient = mock<HttpClient>()
+
+  const projectRoleResponse = ProjectRoleMother.nonHydratedProjectRoles()
+  httpClient.get.calledWith('/api/project-role/latest').mockResolvedValue(projectRoleResponse)
+
+  const projectRolePath = (projectId: Id) => `/api/project/${projectId}/role`
+  httpClient.get.calledWith(projectRolePath(1)).mockResolvedValue(projectRoleResponse)
+
+  return {
+    httpProjectRoleRepository: new HttpProjectRoleRepository(httpClient),
+    httpClient,
+    projectRoleResponse
+  }
+}
