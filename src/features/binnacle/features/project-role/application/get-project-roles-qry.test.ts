51eb593ea19ce65130c5e118829af7e0f5d18c32
Kemil Beltre <kemilbeltre@gmail.com>
2023-05-18 17:41:36 +0200

    test: add get-project-roles-qry

---
+++ src/features/binnacle/features/project-role/application/get-project-roles-qry.test.ts
@@ -0,0 +1,26 @@
+import { mock } from 'jest-mock-extended'
+import { ProjectRoleMother } from 'test-utils/mothers/project-role-mother'
+import { ProjectRoleRepository } from '../domain/project-role-repository'
+import { GetProjectRolesQry } from './get-project-roles-qry'
+
+describe('GetProjectRolesQry', () => {
+  it('should get all the project roles', async () => {
+    const { getProjectRolesQry, nonHydrateProjectRoles } = setup()
+
+    const result = await getProjectRolesQry.internalExecute(1)
+
+    expect(result).toEqual(nonHydrateProjectRoles)
+  })
+})
+
+function setup() {
+  const projectRoleRepository = mock<ProjectRoleRepository>()
+
+  const nonHydrateProjectRoles = ProjectRoleMother.nonHydratedProjectRoles()
+  projectRoleRepository.getAll.calledWith(1).mockResolvedValue(nonHydrateProjectRoles)
+
+  return {
+    getProjectRolesQry: new GetProjectRolesQry(projectRoleRepository),
+    nonHydrateProjectRoles
+  }
+}
