4911086a6dbfa287b65c4923ecc215428c4a48bc
Kemil Beltre <kemilbeltre@gmail.com>
2023-05-19 14:51:20 +0200

    test: add create-activity-cmd

---
+++ src/features/binnacle/features/activity/application/create-activity-cmd.test.ts
@@ -0,0 +1,36 @@
+import { mock } from 'jest-mock-extended'
+import { ActivityRepository } from '../domain/activity-repository'
+import { NewActivity } from '../domain/new-activity'
+import { CreateActivityCmd } from './create-activity-cmd'
+
+describe('CreateActivityCmd', () => {
+  it('', async () => {
+    const { createActivityCmd, activityRepository, newActivity } = setup()
+
+    await createActivityCmd.internalExecute(newActivity)
+
+    expect(activityRepository.create).toBeCalledWith(newActivity)
+  })
+})
+
+function setup() {
+  const activityRepository = mock<ActivityRepository>()
+
+  const newActivity: NewActivity = {
+    description: 'any-description',
+    billable: true,
+    interval: {
+      start: new Date('2000-03-01T09:00:00.000Z'),
+      end: new Date('2000-03-01T13:00:00.000Z')
+    },
+    projectRoleId: 1,
+    imageFile: undefined,
+    hasEvidences: false
+  }
+
+  return {
+    createActivityCmd: new CreateActivityCmd(activityRepository),
+    activityRepository,
+    newActivity
+  }
+}
