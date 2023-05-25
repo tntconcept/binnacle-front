c6ff0748944501590a9c8e1bb6a277601907f3bc
Kemil Beltre <kemilbeltre@gmail.com>
2023-05-25 12:59:00 +0200

    feat(binnacle): add approve activity cmd

---
+++ src/features/binnacle/features/activity/application/approve-activity-cmd.test.ts
@@ -0,0 +1,23 @@
+import { mock } from 'jest-mock-extended'
+import { ActivityRepository } from '../domain/activity-repository'
+import { ApproveActivityCmd } from './approve-activity-cmd'
+
+describe('ApproveActivityCmd', () => {
+  it('should approve an activity', async () => {
+    const { approveActivityCmd, activityRepository } = setup()
+    const id = 1
+
+    await approveActivityCmd.internalExecute(id)
+
+    expect(activityRepository.setApproved).toBeCalledWith(id)
+  })
+})
+
+function setup() {
+  const activityRepository = mock<ActivityRepository>()
+
+  return {
+    approveActivityCmd: new ApproveActivityCmd(activityRepository),
+    activityRepository
+  }
+}
