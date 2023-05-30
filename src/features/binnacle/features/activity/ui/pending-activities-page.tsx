5a9ff56197a92aa0f4e7f01cc23baa200c612676
Andrés <andres.fuentes@autentia.com>
2023-05-30 09:44:50 +0200

    feat: add function to approve an activity

--- src/features/binnacle/features/activity/ui/pending-activities-page.tsx
+++ src/features/binnacle/features/activity/ui/pending-activities-page.tsx
@@ -4,6 +4,10 @@ import { useTranslation } from 'react-i18next'
 import { PageTitle } from 'shared/components/PageTitle'
 import { ActivityModal } from './components/activity-modal/activity-modal'
 import { Activity } from '../domain/activity'
+import { useGetUseCase } from '../../../../../shared/arch/hooks/use-get-use-case'
+import { ApproveActivityCmd } from '../application/approve-activity-cmd'
+import { useResolve } from '../../../../../shared/di/use-resolve'
+import { ActivityErrorMessage } from '../domain/services/activity-error-message'
 
 const PendingActivitiesPage = () => {
   const { t } = useTranslation()
@@ -11,8 +15,21 @@ const PendingActivitiesPage = () => {
   const [activityDate] = useState(new Date())
   const [selectedActivity] = useState<Activity | undefined>()
   const [isLoadingForm, setIsLoadingForm] = useState(false)
+  const { useCase: approveActivityCmd } = useGetUseCase(ApproveActivityCmd)
+  const activityErrorMessage = useResolve(ActivityErrorMessage)
 
   const onCloseActivity = () => {
+    setShowActivityModal(false)
+  }
+
+  const onApprove = async () => {
+    if (selectedActivity) {
+      await approveActivityCmd.execute(selectedActivity.id, {
+        successMessage: t('activity_form.approve_activity'),
+        showToastError: true,
+        errorMessage: activityErrorMessage.get
+      })
+    }
     setShowActivityModal(false)
   }
 
@@ -29,16 +46,13 @@ const PendingActivitiesPage = () => {
           setIsLoadingForm={(isLoading) => setIsLoadingForm(isLoading)}
           employee={'John Doe'}
         >
-          <Button colorScheme="red" variant="ghost">
-            {t('actions.decline')}
-          </Button>
           <Button
             type="button"
             colorScheme="brand"
             variant="solid"
             isLoading={isLoadingForm}
             disabled={false}
-            onClick={() => {}}
+            onClick={() => onApprove()}
           >
             {t('actions.approve')}
           </Button>
