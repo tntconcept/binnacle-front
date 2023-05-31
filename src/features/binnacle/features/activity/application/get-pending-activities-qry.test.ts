f1fd2b95942916f90187fc65317b08d1d93c8e6b
Andrés <andres.fuentes@autentia.com>
2023-05-31 15:17:58 +0200

    test: fix test with new imports

--- src/features/binnacle/features/activity/application/get-pending-activities-qry.test.ts
+++ src/features/binnacle/features/activity/application/get-pending-activities-qry.test.ts
@@ -5,24 +5,32 @@ import { SearchProjectRolesQry } from '../../search/application/search-project-r
 import { ActivityRepository } from '../domain/activity-repository'
 import { ActivitiesWithRoleInformation } from '../domain/services/activities-with-role-information'
 import { GetPendingActivitiesQry } from './get-pending-activities-qry'
+import { ActivitiesWithUserName } from '../domain/services/activities-with-user-name'
+import { GetUsersListQry } from '../../../../user/application/get-users-list-qry'
+import { UserMother } from '../../../../../test-utils/mothers/user-mother'
 
 describe('GetPendingActivitiesQry', () => {
   it('should return pending activities', async () => {
-    const { getPendingActivitiesQry, activities } = setup()
+    const { getPendingActivitiesQry, activitiesUser } = setup()
 
     const result = await getPendingActivitiesQry.internalExecute()
 
-    expect(result).toEqual(activities)
+    expect(result).toEqual(activitiesUser)
   })
 })
 
 function setup() {
   const activityRepository = mock<ActivityRepository>()
+  const getUsersListQry = mock<GetUsersListQry>()
   const searchProjectRolesQry = mock<SearchProjectRolesQry>()
   const activitiesWithRoleInformation = mock<ActivitiesWithRoleInformation>()
+  const activitiesWithUserName = mock<ActivitiesWithUserName>()
 
   const activitiesResponse = ActivityMother.activitiesWithProjectRoleId()
   activityRepository.getPending.mockResolvedValue(activitiesResponse)
+
+  const userList = UserMother.userList()
+  getUsersListQry.execute.mockResolvedValue(userList)
 
   const projectRolesInformation = SearchMother.roles()
   searchProjectRolesQry.execute.calledWith(anyArray()).mockResolvedValue(projectRolesInformation)
@@ -32,15 +40,23 @@ function setup() {
     .calledWith(activitiesResponse, projectRolesInformation)
     .mockReturnValue(activities)
 
+  const activitiesUser = ActivityMother.activitiesPendingWithUserName()
+  activitiesWithUserName.addUserNameToActivities
+    .calledWith(activities, userList)
+    .mockReturnValue(activitiesUser)
+
   return {
     getPendingActivitiesQry: new GetPendingActivitiesQry(
       activityRepository,
       searchProjectRolesQry,
-      activitiesWithRoleInformation
+      getUsersListQry,
+      activitiesWithRoleInformation,
+      activitiesWithUserName
     ),
     activityRepository,
     searchProjectRolesQry,
     activitiesWithRoleInformation,
-    activities
+    activities,
+    activitiesUser
   }
 }
