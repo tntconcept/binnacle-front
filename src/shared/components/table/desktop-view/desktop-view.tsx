c0e1bb8f8eac06dded29dfae00800b58f01e11a1
Andrés <andres.fuentes@autentia.com>
2023-05-31 17:01:11 +0200

    fix(table-desktop-view): add overflow scroll for the table

--- src/shared/components/table/desktop-view/desktop-view.tsx
+++ src/shared/components/table/desktop-view/desktop-view.tsx
@@ -1,4 +1,4 @@
-import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { Box, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { DesktopViewProps } from '../table.types'
 
@@ -10,31 +10,33 @@ const DesktopView: React.FC<DesktopViewProps> = ({
 }) => {
   const { t } = useTranslation()
   return (
-    <Table color={bgColor}>
-      <Thead>
-        <Tr>
-          {columns.map(({ title, key }) => {
-            return title && <Th key={key}>{t(title)}</Th>
-          })}
-        </Tr>
-      </Thead>
-      <Tbody>
-        {dataSource.length === 0 && (
+    <Box overflowX={'auto'}>
+      <Table color={bgColor}>
+        <Thead>
           <Tr>
-            <Td data-testid="empty-desktop-view" colSpan={columns.length}>
-              {t(emptyTableKey)}
-            </Td>
+            {columns.map(({ title, key }) => {
+              return title && <Th key={key}>{t(title)}</Th>
+            })}
           </Tr>
-        )}
-        {dataSource.map((item) => (
-          <Tr key={item.key}>
-            {columns.map(({ key, render, dataIndex }) => (
-              <Td key={key}>{render ? render(item[dataIndex]) : item[dataIndex]}</Td>
-            ))}
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {dataSource.length === 0 && (
+            <Tr>
+              <Td data-testid="empty-desktop-view" colSpan={columns.length}>
+                {t(emptyTableKey)}
+              </Td>
+            </Tr>
+          )}
+          {dataSource.map((item) => (
+            <Tr key={item.key}>
+              {columns.map(({ key, render, dataIndex }) => (
+                <Td key={key}>{render ? render(item[dataIndex]) : item[dataIndex]}</Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
   )
 }
 
