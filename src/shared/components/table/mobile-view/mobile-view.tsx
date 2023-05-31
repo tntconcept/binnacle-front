150856ea586e3ff8070ca86728ab8758434e07a5
Kemil Beltre <kemilbeltre@gmail.com>
2023-05-31 15:52:53 +0200

    feat: add mobile view

--- src/shared/components/table/mobile-view/mobile-view.tsx
+++ src/shared/components/table/mobile-view/mobile-view.tsx
@@ -1,5 +1,84 @@
-const MobileView = () => {
-  return <p>Table mobile view</p>
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Flex,
+  Text
+} from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
+import { ColumnsProps, Item, MobileViewProps } from '../table.types'
+
+const MobileView: React.FC<MobileViewProps> = ({ dataSource, columns, emptyTableKey }) => {
+  const { t } = useTranslation()
+
+  const columnHeadings = (columns: ColumnsProps[]) => {
+    return columns
+      .filter((c) => c.showInMobile)
+      .map(({ title, key }) => {
+        return (
+          <Text key={key} w={100} fontSize="sm" fontWeight="bold" textTransform="uppercase">
+            {title}
+          </Text>
+        )
+      })
+  }
+
+  const accordionButtonData = (columns: ColumnsProps[], item: Item) => {
+    return columns
+      .filter((c) => c.showInMobile)
+      .map(({ key, dataIndex, render }) => {
+        return render ? render(item[dataIndex], key) : item[dataIndex]
+      })
+  }
+
+  const accordionPanelData = (columns: ColumnsProps[], item: Item) => {
+    return columns
+      .filter((c) => !c.showInMobile)
+      .map(({ title, key, dataIndex, render }) => {
+        return render ? (
+          render(item[dataIndex], key)
+        ) : (
+          <Box key={key}>
+            <Text
+              as="span"
+              key={key}
+              w={100}
+              fontSize="sm"
+              fontWeight="bold"
+              textTransform="uppercase"
+            >
+              {`${title}: `}
+            </Text>
+            <Text as="span">{item[dataIndex]}</Text>
+          </Box>
+        )
+      })
+  }
+
+  return (
+    <Box>
+      <Flex textAlign="left" align="center" h={35}>
+        {columnHeadings(columns)}
+      </Flex>
+      {dataSource.length === 0 && <p data-testid="empty-mobile-view">{t(emptyTableKey)}</p>}
+      <Accordion allowToggle allowMultiple>
+        {dataSource.map((item, index) => {
+          return (
+            <AccordionItem key={index}>
+              <AccordionButton px={0}>
+                {accordionButtonData(columns, item)}
+                <AccordionIcon />
+              </AccordionButton>
+              <AccordionPanel px={0}>{accordionPanelData(columns, item)}</AccordionPanel>
+            </AccordionItem>
+          )
+        })}
+      </Accordion>
+    </Box>
+  )
 }
 
 export default MobileView
--- src/shared/components/table/table.tsx
+++ src/shared/components/table/table.tsx
@@ -1,15 +1,17 @@
+import { useColorModeValue } from '@chakra-ui/react'
 import { useIsMobile } from 'shared/hooks'
 import DesktopView from './desktop-view/desktop-view'
 import MobileView from './mobile-view/mobile-view'
 import { TableProps } from './table.types'
 
 const Table: React.FC<TableProps> = ({ dataSource, columns }) => {
+  const bgColor = useColorModeValue('white', undefined)
   const isMobile = useIsMobile()
 
   return isMobile ? (
-    <MobileView />
+    <MobileView dataSource={dataSource} columns={columns} emptyTableKey="any-key" />
   ) : (
-    <DesktopView dataSource={dataSource} columns={columns} emptyTableKey={''} />
+    <DesktopView dataSource={dataSource} columns={columns} emptyTableKey={''} bgColor={bgColor} />
   )
 }
 
