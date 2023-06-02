2b5bf5c78d7c0b0be49580e533dcfc5193227fc3
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-06-02 18:23:57 +0200

    refactor: execute promises in parallel for GetPendingActivitiesQry

--- src/features/binnacle/features/activity/application/get-pending-activities-qry.ts
+++ src/features/binnacle/features/activity/application/get-pending-activities-qry.ts
@@ -1,12 +1,12 @@
 import { Query, UseCaseKey } from '@archimedes/arch'
 import { ACTIVITY_REPOSITORY } from 'shared/di/container-tokens'
 import { inject, singleton } from 'tsyringe'
+import { GetUsersListQry } from '../../../../user/application/get-users-list-qry'
 import { SearchProjectRolesQry } from '../../search/application/search-project-roles-qry'
+import { Activity } from '../domain/activity'
 import type { ActivityRepository } from '../domain/activity-repository'
 import { ActivitiesWithRoleInformation } from '../domain/services/activities-with-role-information'
-import { GetUsersListQry } from '../../../../user/application/get-users-list-qry'
 import { ActivitiesWithUserName } from '../domain/services/activities-with-user-name'
-import { Activity } from '../domain/activity'
 
 @UseCaseKey('GetPendingActivitiesQry')
 @singleton()
@@ -26,9 +26,10 @@ export class GetPendingActivitiesQry extends Query<Activity[]> {
     const projectRoleIds = activitiesResponse.map((a) => a.projectRoleId)
     const uniqueProjectRoleIds = Array.from(new Set(projectRoleIds))
 
-    const projectRolesInformation = await this.searchProjectRolesQry.execute(uniqueProjectRoleIds)
-
-    const usersList = await this.getUsersListQry.execute()
+    const [projectRolesInformation, usersList] = await Promise.all([
+      this.searchProjectRolesQry.execute(uniqueProjectRoleIds),
+      this.getUsersListQry.execute()
+    ])
 
     const activities = this.activitiesWithRoleInformation.addRoleInformationToActivities(
       activitiesResponse,
