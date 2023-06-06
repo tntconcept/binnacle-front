52a06512565f8e472dff87710d57f05d60bfcd50
Andrés <andres.fuentes@autentia.com>
2023-06-06 12:45:33 +0200

    fix: changes in domain

--- src/features/administration/features/project/domain/project-repository.ts
+++ src/features/administration/features/project/domain/project-repository.ts
@@ -4,7 +4,7 @@ import { OrganizationWithStatus } from './organization-status'
 import { ProjectWithDate } from './project-date'
 
 export interface ProjectRepository {
-  getProjects(organizationStatus: OrganizationWithStatus): Promise<Project[]>
+  getProjects(organizationStatus?: OrganizationWithStatus): Promise<Project[]>
   setBlock(projectDate: ProjectWithDate): Promise<void>
   setUnblock(projectId: Id): Promise<void>
 }
