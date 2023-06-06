4d44f97c9d5e6dee71f6c0fdee841d7ba48179a3
Andrés <andres.fuentes@autentia.com>
2023-06-06 16:12:12 +0200

    feat: project modal component

---
+++ src/features/administration/features/project/ui/components/project-modal.schema.ts
@@ -0,0 +1,19 @@
+import i18n from 'shared/i18n/i18n'
+import { object, string } from 'yup'
+import chrono, { parse } from '../../../../../../shared/utils/chrono'
+
+export interface ProjectModalFormSchema {
+  blockDate?: string
+}
+
+export const ProjectModalFormValidationSchema: any = object({
+  blockDate: string()
+    .required(i18n.t('form_errors.field_required'))
+    .test('is-greater', i18n.t('form_errors.date_max_today'), function () {
+      const { blockDate } = this.parent
+      const date = new Date()
+      const blockDateParse = parse(blockDate, chrono.DATE_FORMAT, date)
+      return chrono(blockDateParse).isBefore(date)
+    })
+    .defined()
+}).defined()
---
+++ src/features/administration/features/project/ui/components/project-modal.tsx
@@ -0,0 +1,98 @@
+import { FC } from 'react'
+import {
+  Box,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay
+} from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
+import { useIsMobile } from '../../../../../../shared/hooks'
+import DateField from '../../../../../../shared/components/FormFields/DateField'
+import chrono from '../../../../../../shared/utils/chrono'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup'
+import { ProjectModalFormSchema, ProjectModalFormValidationSchema } from './project-modal.schema'
+import { Project } from '../../domain/project'
+
+type ProjectModalProps = {
+  isOpen: boolean
+  onClose(): void
+  title: string
+  description: string
+  showDateField: boolean
+  onSubmit: SubmitHandler<ProjectModalFormSchema>
+  lastBlockDate?: Date
+  selectedProject?: Project
+  actions?: React.ReactNode
+}
+export const ProjectModal: FC<ProjectModalProps> = (props) => {
+  const {
+    onClose,
+    isOpen = false,
+    title,
+    description,
+    selectedProject,
+    showDateField,
+    lastBlockDate,
+    actions,
+    onSubmit
+  } = props
+  const { t } = useTranslation()
+  const isMobile = useIsMobile()
+  const maxDate = chrono(chrono.now().toISOString()).format('yyyy-MM-dd')
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<ProjectModalFormSchema>({
+    defaultValues: {
+      blockDate: lastBlockDate ? chrono(lastBlockDate.toISOString()).format('yyyy-MM-dd') : maxDate
+    },
+    resolver: yupResolver(ProjectModalFormValidationSchema),
+    mode: 'onChange'
+  })
+
+  return (
+    <Modal
+      onClose={onClose}
+      isOpen={isOpen}
+      scrollBehavior="inside"
+      isCentered={true}
+      size={isMobile ? 'full' : 'xl'}
+    >
+      <ModalOverlay
+        // Prevent focus fighting because of CellBody component
+        // https://github.com/theKashey/focus-lock/#focus-fighting
+        data-no-focus-lock="true"
+      >
+        <ModalContent>
+          <Box as={'form'} onSubmit={handleSubmit(onSubmit)} id={'project-modal-form'}>
+            <ModalHeader>
+              {t(title)} {selectedProject?.name}
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Box fontSize={14}>{t(description)}</Box>
+              {showDateField && (
+                <Box width={'172px'} marginTop={22}>
+                  <DateField
+                    label={t('vacation_form.start_date')}
+                    max={maxDate}
+                    error={errors.blockDate?.message}
+                    {...register('blockDate')}
+                  />
+                </Box>
+              )}
+            </ModalBody>
+            <ModalFooter justifyContent={'space-between'}>{actions}</ModalFooter>
+          </Box>
+        </ModalContent>
+      </ModalOverlay>
+    </Modal>
+  )
+}
