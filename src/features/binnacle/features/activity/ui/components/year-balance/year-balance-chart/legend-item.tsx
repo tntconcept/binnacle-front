c6466d55306f9537b9292ac46050f0838a5efd2d
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-06-12 07:47:19 +0200

    refactor: use PropsWithChildren type for LegendItem

--- src/features/binnacle/features/activity/ui/components/year-balance/year-balance-chart/legend-item.tsx
+++ src/features/binnacle/features/activity/ui/components/year-balance/year-balance-chart/legend-item.tsx
@@ -1,12 +1,12 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
+import { FC, PropsWithChildren } from 'react'
 
 interface Props {
   color: string
   total?: number | string
-  children: React.ReactNode
 }
 
-export const LegendItem = ({ color, total, children }: Props) => {
+export const LegendItem: FC<PropsWithChildren<Props>> = ({ color, total, children }) => {
   return (
     <Flex gap={4} alignItems="center">
       <Box w={4} h={4} backgroundColor={color} />
