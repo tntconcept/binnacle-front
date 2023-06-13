ebff00761abecd1c092510ecaa9cb30b2efd5cb0
goyobjs <goyobujor@gmail.com>
2023-06-13 14:34:07 +0200

    fix: add button to PageWithTitle

--- src/shared/components/page-with-title/page-with-title.tsx
+++ src/shared/components/page-with-title/page-with-title.tsx
@@ -1,23 +1,43 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading } from '@chakra-ui/react'
 import type { FC } from 'react'
+import React from 'react'
 import { useIsMobile, useTitle } from '../../hooks'
 import styles from './page-with-title.module.css'
+import { useTranslation } from 'react-i18next'
 
 interface Props {
   title: string
+  onClickAction?: () => void
 }
 
-export const PageWithTitle: FC<Props> = ({ children, title }) => {
+export const PageWithTitle: FC<Props> = ({ children, title, onClickAction }) => {
+  const { t } = useTranslation()
   const isMobile = useIsMobile()
   useTitle(title)
 
   return (
     <Box mx={8} my={0}>
-      {!isMobile && (
-        <Heading as={'h1'} marginBottom={8} fontSize={28} className={styles.title}>
-          {title}
-        </Heading>
-      )}
+      <Flex justifyContent="space-between" alignItems="flex-start">
+        {!isMobile && (
+          <Heading as={'h1'} marginBottom={8} fontSize={28} className={styles.title}>
+            {title}
+          </Heading>
+        )}
+        {onClickAction && (
+          <Button
+            data-testid="show_activity_modal"
+            onClick={() => onClickAction()}
+            type="button"
+            colorScheme="grey"
+            variant="outline"
+            size="sm"
+            px="29px"
+            py="6px"
+          >
+            {t('activity.create')}
+          </Button>
+        )}
+      </Flex>
       {children}
     </Box>
   )
