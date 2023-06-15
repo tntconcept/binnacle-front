79d6c5d90151d3cfebba29f75c56b6116d0e95ab
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-06-15 10:46:47 +0200

    fix: last end time for new activity on Activities page

--- src/features/binnacle/features/activity/ui/components/activities-list/activities-list-table.tsx
+++ src/features/binnacle/features/activity/ui/components/activities-list/activities-list-table.tsx
@@ -1,89 +1,32 @@
 import { Button } from '@chakra-ui/react'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useExecuteUseCaseOnMount } from 'shared/arch/hooks/use-execute-use-case-on-mount'
-import { useSubscribeToUseCase } from 'shared/arch/hooks/use-subscribe-to-use-case'
 import Table from 'shared/components/table/table'
 import { ColumnsProps } from 'shared/components/table/table.types'
-import chrono from 'shared/utils/chrono'
 import { useIsMobile } from '../../../../../../../shared/hooks'
-import { ApproveActivityCmd } from '../../../application/approve-activity-cmd'
-import { CreateActivityCmd } from '../../../application/create-activity-cmd'
-import { DeleteActivityCmd } from '../../../application/delete-activity-cmd'
-import { GetActivitiesQry } from '../../../application/get-activities-qry'
-import { UpdateActivityCmd } from '../../../application/update-activity-cmd'
 import { Activity } from '../../../domain/activity'
-import { useCalendarContext } from '../../contexts/calendar-context'
 import RemoveActivityButton from '../activity-form/components/remove-activity-button'
-import { activitiesListAdapter, AdaptedActivity } from './activities-list-adapter'
+import { activitiesListAdapter } from './activities-list-adapter'
 
 interface Props {
   onDeleteActivity: () => void
   onOpenActivity: (activity: Activity) => void
+  activities: Activity[]
 }
 
-export const ActivitiesListTable = ({ onOpenActivity, onDeleteActivity }: Props) => {
-  const [tableActivities, setTableActivities] = useState<AdaptedActivity[]>([])
+export const ActivitiesListTable = ({
+  onOpenActivity,
+  onDeleteActivity,
+  activities = []
+}: Props) => {
   const isMobile = useIsMobile()
   const { t } = useTranslation()
-  const { selectedDate } = useCalendarContext()
 
-  const selectedDateInterval = useMemo(() => {
-    const start = chrono(selectedDate).startOf('month').getDate()
-    const end = chrono(selectedDate).endOf('month').getDate()
+  const tableActivities = useMemo(() => {
+    if (!activities) return []
 
-    return { start, end }
-  }, [selectedDate])
-
-  const {
-    isLoading: isLoadingActivities,
-    result: activities = [],
-    executeUseCase: getActivitiesDataQry
-  } = useExecuteUseCaseOnMount(GetActivitiesQry, selectedDateInterval)
-
-  useSubscribeToUseCase(
-    CreateActivityCmd,
-    () => {
-      getActivitiesDataQry(selectedDateInterval)
-    },
-    [selectedDateInterval]
-  )
-
-  useSubscribeToUseCase(
-    UpdateActivityCmd,
-    () => {
-      getActivitiesDataQry(selectedDateInterval)
-    },
-    [selectedDateInterval]
-  )
-
-  useSubscribeToUseCase(
-    DeleteActivityCmd,
-    () => {
-      getActivitiesDataQry(selectedDateInterval)
-    },
-    [selectedDateInterval]
-  )
-
-  useSubscribeToUseCase(
-    ApproveActivityCmd,
-    () => {
-      getActivitiesDataQry(selectedDateInterval)
-    },
-    [selectedDateInterval]
-  )
-
-  useEffect(() => {
-    if (!isLoadingActivities && activities) {
-      const activitiesAdapted = activitiesListAdapter(activities)
-
-      const sortedActivities = activitiesAdapted.sort((a, b) => {
-        return new Date(b.start_date).getTime() - new Date(a.start_date).getTime()
-      })
-
-      setTableActivities(sortedActivities)
-    }
-  }, [isLoadingActivities, activities])
+    return activitiesListAdapter(activities)
+  }, [activities])
 
   const activitiesListColumns: ColumnsProps[] = [
     {
--- src/features/binnacle/features/activity/ui/components/activities-list/activities-list.tsx
+++ src/features/binnacle/features/activity/ui/components/activities-list/activities-list.tsx
@@ -1,7 +1,17 @@
-import { useEffect, useState } from 'react'
+import { SkeletonText } from '@chakra-ui/react'
+import { useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useExecuteUseCaseOnMount } from 'shared/arch/hooks/use-execute-use-case-on-mount'
+import { useSubscribeToUseCase } from 'shared/arch/hooks/use-subscribe-to-use-case'
 import SubmitButton from 'shared/components/FormFields/SubmitButton'
+import chrono from 'shared/utils/chrono'
+import { ApproveActivityCmd } from '../../../application/approve-activity-cmd'
+import { CreateActivityCmd } from '../../../application/create-activity-cmd'
+import { DeleteActivityCmd } from '../../../application/delete-activity-cmd'
+import { GetActivitiesQry } from '../../../application/get-activities-qry'
+import { UpdateActivityCmd } from '../../../application/update-activity-cmd'
 import { Activity } from '../../../domain/activity'
+import { useCalendarContext } from '../../contexts/calendar-context'
 import { ACTIVITY_FORM_ID } from '../activity-form/activity-form'
 import RemoveActivityButton from '../activity-form/components/remove-activity-button'
 import { ActivityModal } from '../activity-modal/activity-modal'
@@ -14,11 +24,56 @@ interface Props {
 
 const ActivitiesList = ({ onCloseActivity, showNewActivityModal }: Props) => {
   const { t } = useTranslation()
-  const activityDate = new Date()
+  const { selectedDate } = useCalendarContext()
   const [selectedActivity, setSelectedActivity] = useState<Activity>()
   const [isLoadingForm, setIsLoadingForm] = useState(false)
   const [showActivityModal, setShowActivityModal] = useState(false)
-  const [lastEndTime] = useState<Date | undefined>()
+  const [lastEndTime, setLastEndTime] = useState<Date | undefined>()
+
+  const selectedDateInterval = useMemo(() => {
+    const start = chrono(selectedDate).startOf('month').getDate()
+    const end = chrono(selectedDate).endOf('month').getDate()
+
+    return { start, end }
+  }, [selectedDate])
+
+  const {
+    isLoading: isLoadingActivities,
+    result: activities = [],
+    executeUseCase: getActivitiesDataQry
+  } = useExecuteUseCaseOnMount(GetActivitiesQry, selectedDateInterval)
+
+  useSubscribeToUseCase(
+    CreateActivityCmd,
+    () => {
+      getActivitiesDataQry(selectedDateInterval)
+    },
+    [selectedDateInterval]
+  )
+
+  useSubscribeToUseCase(
+    UpdateActivityCmd,
+    () => {
+      getActivitiesDataQry(selectedDateInterval)
+    },
+    [selectedDateInterval]
+  )
+
+  useSubscribeToUseCase(
+    DeleteActivityCmd,
+    () => {
+      getActivitiesDataQry(selectedDateInterval)
+    },
+    [selectedDateInterval]
+  )
+
+  useSubscribeToUseCase(
+    ApproveActivityCmd,
+    () => {
+      getActivitiesDataQry(selectedDateInterval)
+    },
+    [selectedDateInterval]
+  )
 
   const onActivityClicked = (activity: Activity) => {
     setSelectedActivity(activity)
@@ -30,23 +85,40 @@ const ActivitiesList = ({ onCloseActivity, showNewActivityModal }: Props) => {
     onCloseActivity()
   }
 
+  const onCreateActivity = () => {
+    const searchActivity = activities
+      .filter((activity) => chrono(activity.interval.start).isSameDay(selectedDate))
+      .reverse()
+      .find((element) => element.projectRole.timeUnit === 'MINUTES')
+    const lastEndTime = searchActivity ? searchActivity.interval.end : undefined
+    setSelectedActivity(undefined)
+    setLastEndTime(lastEndTime)
+    setShowActivityModal(true)
+  }
+
   useEffect(() => {
     if (!showNewActivityModal) return
-
-    setSelectedActivity(undefined)
-    setShowActivityModal(true)
+    onCreateActivity()
   }, [showNewActivityModal])
+
+  if (isLoadingActivities) {
+    return <SkeletonText noOfLines={4} spacing="4" />
+  }
 
   return (
     <>
-      <ActivitiesListTable onOpenActivity={onActivityClicked} onDeleteActivity={onCloseActivity} />
+      <ActivitiesListTable
+        onOpenActivity={onActivityClicked}
+        onDeleteActivity={onCloseActivity}
+        activities={activities}
+      />
       {showActivityModal && (
         <ActivityModal
           isOpen={showActivityModal}
           onClose={onCloseActivityModal}
           onSave={onCloseActivityModal}
           onLoading={setIsLoadingForm}
-          activityDate={selectedActivity?.interval.start || activityDate}
+          activityDate={selectedActivity?.interval.start || new Date()}
           activity={selectedActivity}
           lastEndTime={lastEndTime}
           actions={
