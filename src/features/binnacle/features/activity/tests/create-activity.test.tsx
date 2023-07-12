d7bb3e054b2f6bd6c3bd2f83517773f5bc31ca84
cesalberca <cesalberca@gmail.com>
2023-07-12 11:41:36 +0200

    chore: add basic test

---
+++ src/features/binnacle/features/activity/tests/create-activity.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen, userEvent } from 'test-utils/app-test-utils'
+import { ActivitiesPage } from '../ui/activities-page'
+import { MemoryRouter } from 'react-router-dom'
+import { act } from '@testing-library/react'
+
+describe('Create activity', () => {
+  it('should create a new activity', async () => {
+    setup()
+    userEvent.click(screen.getByTestId('show_activity_modal'))
+
+    act(() => {
+      userEvent.click(screen.getByRole('button', { name: 'activity.create' }))
+    })
+
+    userEvent.type(screen.getByLabelText('activity_form.start_date'), '2021-10-10')
+    userEvent.type(screen.getByLabelText('activity_form.end_date'), '2021-10-10')
+    userEvent.type(screen.getByLabelText('activity_form.description'), 'Description')
+  })
+})
+
+function setup() {
+  return render(
+    <MemoryRouter>
+      <ActivitiesPage />
+    </MemoryRouter>
+  )
+}
