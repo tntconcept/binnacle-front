03ccf1addde0624385269997effb0c3ac350738a
cesalberca <cesalberca@gmail.com>
2023-07-14 16:28:19 +0200

    chore: simplify jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  resetMocks: true,
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  setupFiles: ['./src/test-utils/setup-tests.ts'],
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      statements: 86,
+      branches: 65,
+      functions: 70,
+      lines: 86
+    }
+  },
+  testEnvironment: 'jsdom',
+  coverageReporters: ['html', 'lcov', 'json', 'text'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/src/test-utils/mocks/svg.ts'
+  }
+}
+
+export default config
