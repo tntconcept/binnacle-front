813ab81d90889e046cae50f05bfbcf73fa724cef
cesalberca <cesalberca@gmail.com>
2023-07-14 11:56:13 +0200

    chore: add cypress testing component configuration

---
+++ cypress/support/component.ts
@@ -0,0 +1,17 @@
+import './commands'
+import { mount } from 'cypress/react18'
+import '../../src/index.css'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount
+    }
+  }
+}
+
+Cypress.Commands.add('mount', mount)
+
+// Example use:
+// cy.mount(<MyComponent />)
