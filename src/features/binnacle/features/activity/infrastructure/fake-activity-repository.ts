98065fc6d7eb27c0a68a9813f81770be7bdada2a
Manumartin95 <manuel-harley@hotmail.com>
2023-07-17 12:59:51 +0100

    test: update fake activity repository to use local activities array

--- src/features/binnacle/features/activity/infrastructure/fake-activity-repository.ts
+++ src/features/binnacle/features/activity/infrastructure/fake-activity-repository.ts
@@ -4,9 +4,12 @@ import { ActivityDaySummary } from '../domain/activity-day-summary'
 import { ActivityRepository } from '../domain/activity-repository'
 import { ActivityWithProjectRoleId } from '../domain/activity-with-project-role-id'
 import { TimeSummary } from '../domain/time-summary'
+import { NewActivity } from '../domain/new-activity'
 
 @singleton()
 export class FakeActivityRepository implements ActivityRepository {
+  private activities: ActivityWithProjectRoleId[] = ActivityMother.activitiesWithProjectRoleId()
+
   async getDaysForActivityNaturalDaysPeriod(): Promise<number> {
     return 1
   }
@@ -14,25 +17,43 @@ export class FakeActivityRepository implements ActivityRepository {
   async getDaysForActivityDaysPeriod(): Promise<number> {
     return 1
   }
+
   async getAll(): Promise<ActivityWithProjectRoleId[]> {
-    return ActivityMother.activitiesWithProjectRoleId()
+    console.log(this.activities)
+    return this.activities
   }
 
   getActivityEvidence(): Promise<File> {
     throw new Error('Method not implemented.')
   }
+
   async getActivitySummary(): Promise<ActivityDaySummary[]> {
     return ActivityMother.marchActivitySummary()
   }
-  create(): Promise<ActivityWithProjectRoleId> {
-    throw new Error('Method not implemented.')
+
+  async create(newActivity: NewActivity): Promise<ActivityWithProjectRoleId> {
+    const activity = {
+      ...ActivityMother.activityToActivityWithProjectRoleId(
+        ActivityMother.minutesBillableActivityWithoutEvidence({
+          id: this.activities.length + 1,
+          description: newActivity.description,
+          approvalState: 'PENDING'
+        })
+      )
+    }
+
+    this.activities.push(activity)
+    return activity
   }
+
   update(): Promise<ActivityWithProjectRoleId> {
     throw new Error('Method not implemented.')
   }
+
   delete(): Promise<void> {
     throw new Error('Method not implemented.')
   }
+
   async getTimeSummary(): Promise<TimeSummary> {
     return ActivityMother.timeSummary()
   }
