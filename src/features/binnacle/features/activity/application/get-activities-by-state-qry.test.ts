4445f9f9fa4649a0f52835cfaf3d131a7d151ce7
Manumartin95 <manuel-harley@hotmail.com>
2023-07-19 14:56:26 +0100

    feat: create get activities by state qry

---
+++ src/features/binnacle/features/activity/application/get-activities-by-state-qry.test.ts
@@ -0,0 +1,62 @@
+import { GetUsersListQry } from '../../../../shared/user/application/get-users-list-qry'
+import { mock } from 'jest-mock-extended'
+import { ActivityMother } from '../../../../../test-utils/mothers/activity-mother'
+import { SearchMother } from '../../../../../test-utils/mothers/search-mother'
+import { UserMother } from '../../../../../test-utils/mothers/user-mother'
+import { SearchProjectRolesQry } from '../../search/application/search-project-roles-qry'
+import { ActivityRepository } from '../domain/activity-repository'
+import { ActivitiesWithRoleInformation } from '../domain/services/activities-with-role-information'
+import { ActivitiesWithUserName } from '../domain/services/activities-with-user-name'
+import { GetActivitiesByStateQry } from './get-activities-by-state-qry'
+
+describe('GetPendingActivitiesQry', () => {
+  it('should return pending activities', async () => {
+    const { getPendingActivitiesQry, activitiesUser } = setup()
+
+    const result = await getPendingActivitiesQry.internalExecute({ year: 2023, state: 'PENDING' })
+
+    expect(result).toEqual(activitiesUser)
+  })
+})
+
+function setup() {
+  const activityRepository = mock<ActivityRepository>()
+  const getUsersListQry = mock<GetUsersListQry>()
+  const searchProjectRolesQry = mock<SearchProjectRolesQry>()
+  const activitiesWithRoleInformation = mock<ActivitiesWithRoleInformation>()
+  const activitiesWithUserName = mock<ActivitiesWithUserName>()
+
+  const activitiesResponse = ActivityMother.activitiesWithProjectRoleId()
+  activityRepository.getActivityBasedOnApprovalState.mockResolvedValue(activitiesResponse)
+
+  const userList = UserMother.userList()
+  getUsersListQry.execute.mockResolvedValue(userList)
+
+  const projectRolesInformation = SearchMother.roles()
+  searchProjectRolesQry.execute.mockResolvedValue(projectRolesInformation)
+
+  const activities = ActivityMother.activitiesPending()
+  activitiesWithRoleInformation.addRoleInformationToActivities
+    .calledWith(activitiesResponse, projectRolesInformation)
+    .mockReturnValue(activities)
+
+  const activitiesUser = ActivityMother.activitiesPendingWithUserName()
+  activitiesWithUserName.addUserNameToActivities
+    .calledWith(activities, userList)
+    .mockReturnValue(activitiesUser)
+
+  return {
+    getPendingActivitiesQry: new GetActivitiesByStateQry(
+      activityRepository,
+      searchProjectRolesQry,
+      getUsersListQry,
+      activitiesWithRoleInformation,
+      activitiesWithUserName
+    ),
+    activityRepository,
+    searchProjectRolesQry,
+    activitiesWithRoleInformation,
+    activities,
+    activitiesUser
+  }
+}
---
+++ src/features/binnacle/features/activity/application/get-activities-by-state-qry.ts
@@ -0,0 +1,47 @@
+import { Query, UseCaseKey } from '@archimedes/arch'
+import { GetUsersListQry } from '../../../../shared/user/application/get-users-list-qry'
+import { ACTIVITY_REPOSITORY } from '../../../../../shared/di/container-tokens'
+import { inject, singleton } from 'tsyringe'
+import { SearchProjectRolesQry } from '../../search/application/search-project-roles-qry'
+import { Activity } from '../domain/activity'
+import type { ActivityRepository } from '../domain/activity-repository'
+import { ActivitiesWithRoleInformation } from '../domain/services/activities-with-role-information'
+import { ActivitiesWithUserName } from '../domain/services/activities-with-user-name'
+import { ActivityApprovalStateFilter } from '../domain/activity-approval-state-filter'
+
+interface GetActivitiesByStateParams {
+  year: number
+  state: ActivityApprovalStateFilter
+}
+
+@UseCaseKey('GetPendingActivitiesQry')
+@singleton()
+export class GetActivitiesByStateQry extends Query<Activity[], GetActivitiesByStateParams> {
+  constructor(
+    @inject(ACTIVITY_REPOSITORY) private activityRepository: ActivityRepository,
+    private searchProjectRolesQry: SearchProjectRolesQry,
+    private getUsersListQry: GetUsersListQry,
+    private activitiesWithRoleInformation: ActivitiesWithRoleInformation,
+    private activitiesWithUserName: ActivitiesWithUserName
+  ) {
+    super()
+  }
+
+  async internalExecute({ state, year }: GetActivitiesByStateParams): Promise<Activity[]> {
+    const activitiesResponse = await this.activityRepository.getActivityBasedOnApprovalState(state)
+    const projectRoleIds = activitiesResponse.map((a) => a.projectRoleId)
+    const uniqueProjectRoleIds = Array.from(new Set(projectRoleIds))
+
+    const [projectRolesInformation, usersList] = await Promise.all([
+      this.searchProjectRolesQry.execute({ ids: uniqueProjectRoleIds, year: year }),
+      this.getUsersListQry.execute()
+    ])
+
+    const activities = this.activitiesWithRoleInformation.addRoleInformationToActivities(
+      activitiesResponse,
+      projectRolesInformation
+    )
+
+    return this.activitiesWithUserName.addUserNameToActivities(activities, usersList)
+  }
+}
