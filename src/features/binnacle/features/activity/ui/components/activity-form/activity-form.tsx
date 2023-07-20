6e4e6212ca672a6d81d538ba303c622a19c69979
cesalberca <cesalberca@gmail.com>
2023-07-20 16:43:25 +0200

    fix: change to ts-ignore

--- src/features/binnacle/features/activity/ui/components/activity-form/activity-form.tsx
+++ src/features/binnacle/features/activity/ui/components/activity-form/activity-form.tsx
@@ -162,7 +162,7 @@ export const ActivityForm: FC<ActivityFormProps> = (props) => {
     recentProjectRole,
     showRecentRole,
     file
-    // @ts-expect-error -- TODO: Remove when updating to latest version of react-hook-form
+    // @ts-ignore -- TODO: Remove when updating to latest version of react-hook-form
   ] = useWatch({
     control: control,
     name: [
