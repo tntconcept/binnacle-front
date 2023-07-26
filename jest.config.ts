9be691f40c96eaec81c6b4413cc42feeb85ca591
cesalberca <cesalberca@gmail.com>
2023-07-26 11:49:47 +0200

    chore: add svg support in jest

--- jest.config.ts
+++ jest.config.ts
@@ -7,11 +7,12 @@ const config: Config = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   coverageReporters: ['json', 'text', 'lcov', 'clover'],
-  setupFiles: ['./src/test-utils/setup-tests.ts'],
+  setupFiles: ['<rootDir>/src/test-utils/setup-tests.ts'],
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.(css|sass|scss)$': 'identity-obj-proxy'
+    '\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/src/test-utils/mocks/svg.ts'
   },
   modulePathIgnorePatterns: ['cypress']
 }
