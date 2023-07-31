931d605da5c37aef6a756a41a1610c044be17b37
cesalberca <cesalberca@gmail.com>
2023-07-31 16:05:29 +0200

    chore: use custom extensions

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,9 +5,10 @@ export default defineConfig({
   viewportHeight: 960,
   e2e: {
     baseUrl: 'http://localhost:3000/tnt',
-    specPattern: 'cypress/tests/**/*test.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/tests/**/*.e2e.{js,jsx,ts,tsx}'
   },
   component: {
+    specPattern: 'src/**/*.int.{js,jsx,ts,tsx}',
     devServer: {
       framework: 'react',
       bundler: 'vite'
