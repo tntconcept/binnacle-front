aab337b419b75943a3f96c03f2104ee9897c33ac
cesalberca <cesalberca@gmail.com>
2023-07-31 11:22:43 +0200

    chore: fix import

--- src/features/binnacle/features/activity/ui/calendar-desktop/activities-calendar/calendar-cell/cell-header/leisure/vacation.tsx
+++ src/features/binnacle/features/activity/ui/calendar-desktop/activities-calendar/calendar-cell/cell-header/leisure/vacation.tsx
@@ -1,21 +1,26 @@
-import { FC } from 'react'
+import { FC, forwardRef } from 'react'
 import { Leisure } from './leisure'
 import { CellHeaderProps } from '../cell-header-props'
 import { useTranslation } from 'react-i18next'
 
 type Props = CellHeaderProps
 
-export const Vacation: FC<Props> = ({ date, selectedMonth, time, activities }) => {
-  const { t } = useTranslation()
+export const Vacation: FC<Props> = forwardRef<HTMLButtonElement, Props>(
+  ({ date, selectedMonth, time, activities }, ref) => {
+    const { t } = useTranslation()
 
-  return (
-    <Leisure
-      date={date}
-      time={time}
-      headerColor={'blue.400'}
-      description={t('vacation.title')}
-      selectedMonth={selectedMonth}
-      activities={activities}
-    />
-  )
-}
+    return (
+      <Leisure
+        ref={ref}
+        date={date}
+        time={time}
+        headerColor={'blue.400'}
+        description={t('vacation.title')}
+        selectedMonth={selectedMonth}
+        activities={activities}
+      />
+    )
+  }
+)
+
+Vacation.displayName = 'Vacation'
