bca6860375f579a044058830f31fcff5cf250a7d
cesalberca <cesalberca@gmail.com>
2023-08-02 11:15:02 +0200

    chore: add env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_SUBDIRECTORY_PATH: string
+  readonly VITE_API_BASE_URL?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
