61590273b7fcc967818a5a4eb63f99902357dcbc
cesalberca <cesalberca@gmail.com>
2023-08-16 15:08:09 +0200

    chore: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vitest/config'
+import svgr from 'vite-plugin-svgr'
 
 process.env.TZ = 'UTC'
 
 export default defineConfig({
+  plugins: [svgr()],
   test: {
     environment: 'jsdom',
     setupFiles: './src/test-utils/setup-tests.ts'
