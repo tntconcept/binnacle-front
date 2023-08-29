eaf809de863b80b8ee4ee419ad9c51f778684bac
Manumartin95 <manuel-harley@hotmail.com>
2023-08-29 16:03:34 +0100

    feat: add control to handle error message and do petition after clicking on the icon

--- src/features/binnacle/features/activity/ui/components/pending-activities/evidence-icon/evidence-icon.tsx
+++ src/features/binnacle/features/activity/ui/components/pending-activities/evidence-icon/evidence-icon.tsx
@@ -1,9 +1,11 @@
 import { PaperClipIcon } from '@heroicons/react/24/outline'
-import { FC, useEffect, useState } from 'react'
-import { Spinner } from '@chakra-ui/react'
+import { FC, useState } from 'react'
 import { Id } from '@archimedes/arch'
 import { useGetUseCase } from '../../../../../../../../shared/arch/hooks/use-get-use-case'
 import { GetActivityEvidenceQry } from '../../../../application/get-activity-image-qry'
+import { Spinner } from '@chakra-ui/react'
+import { useResolve } from '../../../../../../../../shared/di/use-resolve'
+import { ActivityErrorMessage } from '../../../../domain/services/activity-error-message'
 
 const supportedImageTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
 const supportedImagesSet = new Set(supportedImageTypes)
@@ -15,43 +17,40 @@ interface Props {
 
 export const EvidenceIcon: FC<Props> = (props) => {
   const { activityId, evidenceKey } = props
+  const activityErrorMessage = useResolve(ActivityErrorMessage)
   const { useCase: getActivityEvidenceQry } = useGetUseCase(GetActivityEvidenceQry)
 
-  const [isLoadingEvidence, setIsLoadingEvidence] = useState(true)
-  const [evidence, setEvidence] = useState<File | undefined>(undefined)
+  const [showSpinner, setShowSpinner] = useState(false)
 
-  useEffect(() => {
-    getActivityEvidenceQry.execute(activityId).then((evidence) => {
-      setEvidence(evidence)
-      setIsLoadingEvidence(false)
-    })
-  }, [activityId])
+  const handlePreview = () => {
+    getActivityEvidenceQry
+      .execute(activityId, { showToastError: true, errorMessage: activityErrorMessage.get })
+      .then((file) => {
+        if (!supportedImagesSet.has(file.type)) {
+          window.open(URL.createObjectURL(file), '_blank')
+          return
+        }
 
-  const handlePreview = (file: File) => {
-    if (!supportedImagesSet.has(file.type)) {
-      window.open(URL.createObjectURL(file), '_blank')
-      return
-    }
+        const image = new Image()
+        image.src = URL.createObjectURL(file)
+        const newWindow = window.open('', '_blank')
 
-    const image = new Image()
-    image.src = URL.createObjectURL(file)
-    const newWindow = window.open('', '_blank')
-
-    if (newWindow !== null) {
-      newWindow.document.write(image.outerHTML)
-    }
+        if (newWindow !== null) {
+          newWindow.document.write(image.outerHTML)
+        }
+      })
+      .finally(() => setShowSpinner(false))
   }
 
   return (
     <>
-      {isLoadingEvidence ? (
+      {showSpinner ? (
         <Spinner />
       ) : (
         <PaperClipIcon
           onClick={() => {
-            if (evidence !== undefined) {
-              handlePreview(evidence)
-            }
+            setShowSpinner(true)
+            handlePreview()
           }}
           style={{ cursor: 'pointer' }}
           key={'icon' + evidenceKey}
