5232564031a6724d6205b7889c2bce754ab91a49
Manumartin95 <manuel-harley@hotmail.com>
2023-08-30 11:35:20 +0100

    feat: add cypress test to create activity setting time variables on blur

--- src/features/binnacle/features/activity/tests/create-activity.page.int.tsx
+++ src/features/binnacle/features/activity/tests/create-activity.page.int.tsx
@@ -9,6 +9,21 @@ describe('Create activity', () => {
 
     cy.findByText('Billable project').should('exist')
   })
+
+  it('should create a new activity using tab', () => {
+    setup()
+    cy.findByTestId('show_activity_modal').click()
+    cy.get('[data-testid="startTime_field"]').clear()
+    cy.get('[data-testid="startTime_field"]').type('12:0')
+    cy.get('[data-testid="endTime_field"]').clear()
+    cy.get('[data-testid="endTime_field"]').type('15')
+
+    cy.findByLabelText('Description').type('Hello world', { force: true })
+
+    cy.findByRole('button', { name: 'Save' }).click()
+
+    cy.findAllByText('Billable project').should('have.length', 2)
+  })
 })
 
 function setup() {
