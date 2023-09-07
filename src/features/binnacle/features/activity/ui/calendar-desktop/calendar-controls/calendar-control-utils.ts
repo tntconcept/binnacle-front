919eb488adc5566ae88939c3c7ae877505e0e713
Manumartin95 <manuel-harley@hotmail.com>
2023-09-07 12:45:58 +0100

    feat: remove console log

--- src/features/binnacle/features/activity/ui/calendar-desktop/calendar-controls/calendar-control-utils.ts
+++ src/features/binnacle/features/activity/ui/calendar-desktop/calendar-controls/calendar-control-utils.ts
@@ -4,7 +4,6 @@ export const handleKeyPressWhenModalIsNotOpened = (
   handler: () => void
 ) => {
   const isModalOpened = document.querySelector('[id^=chakra-modal]') !== null
-  console.log(document.querySelector('[id^=chakra-modal]'))
   if (isModalOpened) return
   if (pressedKey === controlledKey) handler()
 }
