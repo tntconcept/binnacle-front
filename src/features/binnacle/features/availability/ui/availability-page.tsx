1a90a465e40f7df88bfa845a330bb0fa635cd4b4
Manumartin95 <manuel-harley@hotmail.com>
2023-09-18 16:29:51 +0200

    feat: create availability table

--- src/features/binnacle/features/availability/ui/availability-page.tsx
+++ src/features/binnacle/features/availability/ui/availability-page.tsx
@@ -1,7 +1,16 @@
 import { FC } from 'react'
+import { PageWithTitle } from '../../../../../shared/components/page-with-title/page-with-title'
+import { useTranslation } from 'react-i18next'
+import { AvailabilityTable } from './components/availability-table'
 
 const AvailabilityPage: FC = () => {
-  return <h1>Hello world</h1>
+  const { t } = useTranslation()
+
+  return (
+    <PageWithTitle title={t('pages.availability')}>
+      <AvailabilityTable />
+    </PageWithTitle>
+  )
 }
 
 export default AvailabilityPage
---
+++ src/features/binnacle/features/availability/ui/components/availability-table.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react'
+import { useCalendarContext } from '../../../activity/ui/contexts/calendar-context'
+import { Table, Th, Thead, Tr } from '@chakra-ui/react'
+import { chrono } from '../../../../../../shared/utils/chrono'
+import { AvailabilityTableCellHeader } from './availability-table-cell-header'
+
+export const AvailabilityTable: FC = () => {
+  const { selectedDate } = useCalendarContext()
+  const daysOfMonth = chrono(chrono(selectedDate).startOf('month').getDate()).eachDayUntil(
+    chrono(selectedDate).endOf('month').getDate()
+  )
+  const tableHeaders = (
+    <Thead>
+      <Tr>
+        <Th></Th>
+        {daysOfMonth.map((day, index) => (
+          <Th key={index}>
+            <AvailabilityTableCellHeader day={day}></AvailabilityTableCellHeader>
+          </Th>
+        ))}
+      </Tr>
+    </Thead>
+  )
+
+  return <Table>{tableHeaders}</Table>
+}
