6ff6d6c5e04844ac7fa5bd3917dce468472c9e35
Manumartin95 <manuel-harley@hotmail.com>
2023-09-18 16:40:19 +0200

    feat: add logic to set day of week and day of month

--- src/features/binnacle/features/availability/ui/components/availability-table-cell-header.tsx
+++ src/features/binnacle/features/availability/ui/components/availability-table-cell-header.tsx
@@ -1,9 +1,24 @@
 import { FC } from 'react'
+import { Box, Text } from '@chakra-ui/react'
+import { chrono } from '../../../../../../shared/utils/chrono'
+import { getWeekdaysName } from '../../../activity/utils/get-weekdays-name'
 
 interface Props {
   day: Date
 }
 
-export const AvailabilityTableCellHeader: FC<Props> = (props) => {
-  return <h1>{props.day.getDate()}</h1>
+const weekDays = getWeekdaysName()
+
+const getWeekDay = (date: Date) => {
+  const weekDay = chrono(date).get('weekday')
+  return weekDay === 0 ? 7 : weekDay
+}
+
+export const AvailabilityTableCellHeader: FC<Props> = ({ day }) => {
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Text>{weekDays[getWeekDay(day) - 1]}</Text>
+      <Text>{day.getDate()}</Text>
+    </Box>
+  )
 }
