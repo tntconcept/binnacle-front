082a3f0c2d5c68444c57ec89ea5c0fee1e0cd535
Manumartin95 <manuel-harley@hotmail.com>
2023-10-10 13:23:06 +0200

    feat: update conditions to navigate with keypress

--- src/features/binnacle/features/activity/ui/calendar-desktop/calendar-controls/calendar-control-utils.ts
+++ src/features/binnacle/features/activity/ui/calendar-desktop/calendar-controls/calendar-control-utils.ts
@@ -1,9 +1,10 @@
-export const handleKeyPressWhenBodyIsNotFocused = (
+export const handleKeyPressWhenModalIsNotOpenedOrInputIsNotFocused = (
   pressedKey: string,
   controlledKey: string,
   handler: () => void
 ) => {
-  const hasFocusInBody = document.activeElement?.tagName === 'BODY'
-  if (!hasFocusInBody) return
+  const hasFocusOnInput = document.activeElement?.tagName === 'INPUT'
+  const isModalOpened = document.querySelector('[id^=chakra-modal]') !== null
+  if (hasFocusOnInput || isModalOpened) return
   if (pressedKey === controlledKey) handler()
 }
