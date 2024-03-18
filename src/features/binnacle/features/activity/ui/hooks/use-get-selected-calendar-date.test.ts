f46e3451889b50821597531072434f6fadca823e
Manumartin95 <manuelmartinra@gmail.com>
2024-03-18 10:17:01 +0100

    fix: test issue with dates

--- src/features/binnacle/features/activity/ui/hooks/use-get-selected-calendar-date.test.ts
+++ src/features/binnacle/features/activity/ui/hooks/use-get-selected-calendar-date.test.ts
@@ -13,7 +13,7 @@ describe('useGetSelectedCalendarDate', () => {
   })
 
   it('should return the last month of the selected year', () => {
-    const pastDate = new Date(2020, 6, 1) // Jun 1, 2020
+    const pastDate = new Date(new Date().getFullYear() - 1, 6, 1) // Jun 1, 2020
 
     const { result } = renderHook(() => useGetSelectedCalendarDate(pastDate))
 
@@ -22,7 +22,7 @@ describe('useGetSelectedCalendarDate', () => {
   })
 
   it('should return the first month of the selected year', () => {
-    const nextDate = new Date(2024, 7, 2) // July 2, 2024
+    const nextDate = new Date(new Date().getFullYear() + 1, 7, 2) // July 2, 2024
 
     const { result } = renderHook(() => useGetSelectedCalendarDate(nextDate))
 
