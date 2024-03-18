92005c587f9c25d3d00e5f5c4b6b02300bbb735e
Manumartin95 <manuelmartinra@gmail.com>
2024-03-18 11:08:58 +0100

    feat: implement login to repository

--- src/features/auth/infrastructure/http-auth-repository.ts
+++ src/features/auth/infrastructure/http-auth-repository.ts
@@ -1,14 +1,20 @@
 import { HttpClient } from '../../../shared/http/http-client'
 import { singleton } from 'tsyringe'
 import { AuthRepository } from '../domain/auth-repository'
+import { UserCredentials } from '../domain/user-credentials'
 
 @singleton()
 export class HttpAuthRepository implements AuthRepository {
   protected static logoutPath = '/logout'
+  protected static loginPath = '/login'
 
   constructor(private httpClient: HttpClient) {}
 
   async logout(): Promise<void> {
     return this.httpClient.post<void>(HttpAuthRepository.logoutPath)
+  }
+
+  async login(userCredentials: UserCredentials): Promise<void> {
+    return this.httpClient.post(HttpAuthRepository.loginPath, { data: userCredentials })
   }
 }
