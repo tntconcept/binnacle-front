af6bc4e2393551d2760cbae483799a52e3e3c7c6
Manumartin95 <manuelmartinra@gmail.com>
2024-04-09 12:03:02 +0200

    test: add login form test

--- src/features/auth/ui/components/login-form/login-form.test.tsx
+++ src/features/auth/ui/components/login-form/login-form.test.tsx
@@ -1,5 +1,8 @@
-import { render, screen, waitFor } from '../../../../../test-utils/render'
+import { render, screen, userEvent, waitFor } from '../../../../../test-utils/render'
 import { LoginForm } from './login-form'
+import { act } from '@testing-library/react'
+import { useGetUseCase } from '../../../../../shared/arch/hooks/use-get-use-case'
+import { useAuthContext } from '../../../../../shared/contexts/auth-context'
 
 jest.mock('../../../../version/ui/components/app-version', () => {
   return {
@@ -7,6 +10,11 @@ jest.mock('../../../../version/ui/components/app-version', () => {
     AppVersion: () => <div>AppVersion</div>
   }
 })
+
+jest.mock('../../../../../shared/arch/hooks/use-get-use-case')
+jest.mock('../../../../../shared/contexts/auth-context')
+
+const useCaseSpy = jest.fn()
 
 describe('LoginForm', () => {
   const setup = () => {
@@ -23,5 +31,22 @@ describe('LoginForm', () => {
     await waitFor(() => {
       expect(screen.getByText('login_page.welcome_message')).toBeInTheDocument()
     })
+  })
+  it('should login', async () => {
+    ;(useGetUseCase as jest.Mock).mockImplementation(() => ({
+      isLoading: false,
+      executeUseCase: useCaseSpy
+    }))
+    ;(useAuthContext as jest.Mock).mockImplementation(() => ({ checkLoggedUser: jest.fn() }))
+
+    setup()
+
+    await act(async () => {
+      await userEvent.type(screen.getByLabelText('login_page.username_field'), 'user')
+      await userEvent.type(screen.getByLabelText('login_page.password_field'), 'password')
+      await userEvent.click(screen.getByRole('button', { name: 'login_page.login' }))
+    })
+
+    expect(useCaseSpy).toHaveBeenCalledWith({ password: 'password', username: 'user' })
   })
 })
